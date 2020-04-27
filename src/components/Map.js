5f5738d23a2f2c38ba27db1d229e8857e9b7c86c
Peter Main <toiletbleach@gmail.com>
2020-04-27 16:10:45 +0100

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,16 +5,16 @@ const MAPBOX_TOKEN =
 
 
 const Map = ({ coords }) => {
-  let url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/"
+  let url = "https://api.mapbox.com/styles/v1/petermain/ck9edce300f8z1iqsmguy9v3b/static/"
   coords.map((c) => {
     url += `pin-l+EF4C60(${c.lng},${c.lat}),`
   })
   url = url.substring(0, url.length - 1);
   let center = Math.floor(coords.length / 2)
-  url += `/${coords[center].lng},${coords[center].lat},11/1024x768?access_token=${MAPBOX_TOKEN}`
+  url += `/${coords[center].lng},${coords[center].lat},11/850x400@2x?access_token=${MAPBOX_TOKEN}`
   return (
     <img
-      style={{ width: '100%', height: 400 }}
+      style={{ width: '100%'}}
       src={url}
     />
   )

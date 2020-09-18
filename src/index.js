d263a0edd84d97df010cd3b18bdd38cc874f58e5
Andrew Allen <allenan@helium.com>
2020-09-17 17:05:15 -0700

    add google analytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import Analytics from 'react-router-ga'
 import './index.css'
 import Explorer from './Explorer'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Explorer />
+    <Analytics id="UA-52432858-12">
+      <Explorer />
+    </Analytics>
   </BrowserRouter>,
   document.getElementById('root'),
 )

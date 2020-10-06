249060254daac11cd9792449ec30b53263254f6d
Daniel James <danielcolinjames@gmail.com>
2020-10-06 08:49:07 -0700

    Disable "Next Block" button for current block (#18)
    * Disable next block button for current block
    
    * Extract block height to HOC

--- src/components/AppLayout/BlockHeight.js
+++ src/components/AppLayout/BlockHeight.js
@@ -3,47 +3,29 @@ import { Typography, Tooltip, Spin } from 'antd'
 import { CodeSandboxOutlined } from '@ant-design/icons'
 import Client from '@helium/http'
 import Block from '../../images/block.svg'
+import withBlockHeight from '../withBlockHeight'
 
 class BlockHeight extends Component {
-  state = {
-    height: 0,
-    loading: true,
-  }
-
-  componentDidMount() {
-    this.client = new Client()
-    this.loadBlockHeight()
-    window.setInterval(this.loadBlockHeight, 30000)
-  }
-
-  loadBlockHeight = async () => {
-    try {
-      const blocks = await this.client.blocks.list()
-      const [{ height }] = await blocks.take(1)
-      this.setState({ height, loading: false })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   render() {
-    const { height, loading } = this.state
     return (
       <Tooltip placement="bottomRight" title="Current Block Height">
         <a
-          href={`/blocks/${height}`}
+          href={`/blocks/${this.props.height}`}
           style={{ minWidth: 127, textAlign: 'center', fontSize: 17 }}
         >
           <img
             style={{ marginRight: 5, position: 'relative', top: '-1px' }}
             src={Block}
+            alt=""
           />
-          {!loading && height.toLocaleString()}
-          {loading && <Spin size="small" />}
+          {!this.props.heightLoading && this.props.height.toLocaleString()}
+          {this.props.heightLoading && <Spin size="small" />}
         </a>
       </Tooltip>
     )
   }
 }
 
-export default BlockHeight
+const WrappedBlockHeight = withBlockHeight(BlockHeight)
+
+export default WrappedBlockHeight

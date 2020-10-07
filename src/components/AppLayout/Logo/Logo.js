c73b6c5a2daf72bdeca956b43346789493acd48e
Daniel James <danielcolinjames@gmail.com>
2020-10-06 17:22:41 -0700

    Change alt tag

--- src/components/AppLayout/Logo/Logo.js
+++ src/components/AppLayout/Logo/Logo.js
@@ -4,8 +4,18 @@ import logoMobile from './helium-logo-symbol-white.svg'
 
 const Logo = () => (
   <div className="logo-container" style={{ float: 'left' }}>
-    <img src={logo} className="logo-full" style={{ height: 30 }} alt="logo-full"/>
-    <img src={logoMobile} className="logo-small" style={{ height: 30 }} alt="logo-small"/>
+    <img
+      src={logo}
+      className="logo-full"
+      style={{ height: 30 }}
+      alt="Helium logo"
+    />
+    <img
+      src={logoMobile}
+      className="logo-small"
+      style={{ height: 30 }}
+      alt="Helium logo"
+    />
   </div>
 )
 

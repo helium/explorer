6a9ee191cf238b5844c65a60daaf10617d65c6a3
Daniel James <danielcolinjames@gmail.com>
2020-10-27 19:34:14 -0700

    Fix txn map bug and use fallback lat/lon names

--- components/PocMapbox.js
+++ components/PocMapbox.js
@@ -78,13 +78,22 @@ const styles = {
 }
 
 const PocMapbox = ({ path, showWitnesses }) => {
+  console.log(path)
   return (
     <Mapbox
       style={`mapbox://styles/petermain/cjyzlw0av4grj1ck97d8r0yrk`}
       container="map"
       center={[
-        path[0].challengee_lon ? path[0].challengee_lon : 0,
-        path[0].challengee_lat ? path[0].challengee_lat : 0,
+        path[0].challengee_lon
+          ? path[0].challengee_lon
+          : path[0].challengeeLon
+          ? path[0].challengeeLon
+          : 0,
+        path[0].challengee_lat
+          ? path[0].challengee_lat
+          : path[0].challengeeLat
+          ? path[0].challengeeLat
+          : 0,
       ]}
       containerStyle={{
         height: '600px',
@@ -108,8 +117,16 @@ const PocMapbox = ({ path, showWitnesses }) => {
               }
               anchor="center"
               coordinates={[
-                p.challengee_lon ? p.challengee_lon : 0,
-                p.challengee_lat ? p.challengee_lat : 0,
+                p.challengee_lon
+                  ? p.challengee_lon
+                  : p.challengeeLon
+                  ? p.challengeeLon
+                  : 0,
+                p.challengee_lat
+                  ? p.challengee_lat
+                  : p.challengeeLat
+                  ? p.challengeeLat
+                  : 0,
               ]}
             >
               <span style={{ color: 'white', fontSize: '8px' }}>{idx + 1}</span>
@@ -129,11 +146,30 @@ const PocMapbox = ({ path, showWitnesses }) => {
             >
               <Feature
                 coordinates={[
-                  [p.challengee_lon, p.challengee_lat],
+                  [
+                    p.challengee_lon
+                      ? p.challengee_lon
+                      : p.challengeeLon
+                      ? p.challengeeLon
+                      : 0,
+                    p.challengee_lat
+                      ? p.challengee_lat
+                      : p.challengeeLat
+                      ? p.challengeeLat
+                      : 0,
+                  ],
                   path[idx + 1]
                     ? [
-                        path[idx + 1].challengee_lon,
-                        path[idx + 1].challengee_lat,
+                        path[idx + 1].challengee_lon
+                          ? path[idx + 1].challengee_lon
+                          : path[idx + 1].challengeeLon
+                          ? path[idx + 1].challengeeLon
+                          : 0,
+                        path[idx + 1].challengee_lat
+                          ? path[idx + 1].challengee_lat
+                          : path[idx + 1].challengeeLat
+                          ? path[idx + 1].challengeeLat
+                          : 0,
                       ]
                     : [false],
                 ]}
@@ -173,7 +209,18 @@ const PocMapbox = ({ path, showWitnesses }) => {
                       <Feature
                         coordinates={[
                           [h3ToGeo(w.location)[1], h3ToGeo(w.location)[0]],
-                          [p.challengee_lon, p.challengee_lat],
+                          [
+                            p.challengee_lon
+                              ? p.challengee_lon
+                              : p.challengeeLon
+                              ? p.challengeeLon
+                              : 0,
+                            p.challengee_lat
+                              ? p.challengee_lat
+                              : p.challengeeLat
+                              ? p.challengeeLat
+                              : 0,
+                          ],
                         ]}
                       />
                     </Layer>

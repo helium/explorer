7cc70f72726fde96027dee621e535ceaa0d0512f
Daniel James <danielcolinjames@gmail.com>
2020-11-11 12:10:28 -0800

    Fix meta tag wrong name

--- components/AppLayout/MetaTags.js
+++ components/AppLayout/MetaTags.js
@@ -15,7 +15,7 @@ const MetaTags = ({ title, description, openGraphImageFullUrl, url }) => {
     <>
       <Head>
         {/* General Meta Tags */}
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <link rel="icon" href="https://explorer.helium.com/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />

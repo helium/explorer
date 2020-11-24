ef957f37595e4a90e09c0203885ccb05ae3054a2
Daniel James <danielcolinjames@gmail.com>
2020-11-23 16:55:15 -0800

    Add coverage map to Explorer (#54)
    * Start adding coverage map
    
    * Add mini coverage map to home page
    
    * WIP: Start adding /coverage page functionality
    
    * Restructure app for Next.js
    
    * Fix search bar to use Next router
    
    * Add dynamic routes to block detail page
    
    * Move txn view page file
    
    * Update imports and packages
    
    * Add dynamic routes for account detail page
    
    * Start adding dynamic routes for hotspot page
    
    * Make all page types work with dynamic routes
    
    * Convert all internal <a>s to <Link>s
    
    * Update README
    
    * Make project production-ready
    
    * Update package.json
    
    Co-authored-by: Andrew Allen <allenan@helium.com>
    
    * Add default meta tags for all pages
    
    * Get /coverage closer to working
    
    * Remove unused build-breaking file
    
    * Styling fixes
    
    * Adjust look of minimap points on home page
    
    * Replace nav bar block height with /coverage link
    
    * Make /coverage responsive
    
    * Add zoom in and out buttons to coverage map
    
    * Improve styling of nav bar map button
    
    * Add hotspot page link for selected hotspot in map
    
    * Change hotspot detail link icon
    
    * Open hotspot detail link in new tab
    
    * Make manual zooms work after Mapbox flyTo event
    
    * ISR hotspot load for coverage map
    
    * Use helium-js instead of fetch
    
    * Fix button text
    
    * Update yarn.lock
    
    * Remove fade from home page to avoid flicker
    
    * use mapbox datasets, sidebar infinite scroll
    
    * remove unneeded packages
    
    * use env var for mapbox public key
    
    * add new env vars to app.json
    
    * add algolia to coverage search
    
    * remove unneeded network components
    
    Co-authored-by: Andrew Allen <allenan@helium.com>

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,15 @@ import { Row, Col } from 'antd'
 import BlocksList from '../components/BlocksList'
 import AppLayout, { Content } from '../components/AppLayout'
 import { Typography } from 'antd'
-import Fade from 'react-reveal/Fade'
+import dynamic from 'next/dynamic'
+
+const MiniCoverageMap = dynamic(
+  () => import('../components/CoverageMap/MiniCoverageMap'),
+  {
+    ssr: false,
+    loading: () => <div style={{ height: '500px' }} />,
+  },
+)
 
 const { Title } = Typography
 
@@ -48,106 +56,125 @@ function Index(props) {
             </Title>
           </div>
 
-          <Fade top>
-            <div
-              style={{
-                background: '#3F416D',
-                borderRadius: 10,
-              }}
-              className="summary-header"
-            >
-              <Row>
-                <Col lg={12}>
-                  <h3
-                    style={{
-                      marginBottom: 20,
-                      color: '#1890ff',
-                      fontSize: 14,
-                    }}
-                  >
-                    Blockchain Stats
-                  </h3>
-                  <p className="stat">
-                    <span>Block Height:</span>
-                    {height.toLocaleString()}
-                  </p>
-                  <p className="stat">
-                    <span>Total Hotspots:</span>
-                    {totalHotspots.toLocaleString()}
-                  </p>
-                  <p className="stat">
-                    <span>LongFi data (30d):</span>
-                    {((dataCredits * 24) / 10e8).toLocaleString()} GB
-                  </p>
-                  <p className="stat">
-                    <span>Avg Election Time (24hr):</span>
-                    {Math.floor(electionTime / 60)}m
-                  </p>
-                  <p className="stat">
-                    <span>Avg Block Time (24hr):</span>
-                    {Math.round(blockTime * 10) / 10}s
-                  </p>
-                </Col>
+          <div
+            style={{
+              background: '#3F416D',
+              borderRadius: 10,
+            }}
+            className="summary-header"
+          >
+            <Row>
+              <Col lg={12}>
+                <h3
+                  style={{
+                    marginBottom: 20,
+                    color: '#1890ff',
+                    fontSize: 14,
+                  }}
+                >
+                  Blockchain Stats
+                </h3>
+                <p className="stat">
+                  <span>Block Height:</span>
+                  {height.toLocaleString()}
+                </p>
+                <p className="stat">
+                  <span>Total Hotspots:</span>
+                  {totalHotspots.toLocaleString()}
+                </p>
+                <p className="stat">
+                  <span>LongFi data (30d):</span>
+                  {((dataCredits * 24) / 10e8).toLocaleString()} GB
+                </p>
+                <p className="stat">
+                  <span>Avg Election Time (24hr):</span>
+                  {Math.floor(electionTime / 60)}m
+                </p>
+                <p className="stat">
+                  <span>Avg Block Time (24hr):</span>
+                  {Math.round(blockTime * 10) / 10}s
+                </p>
+              </Col>
 
-                <Col lg={12}>
-                  <h3
-                    style={{
-                      marginBottom: 20,
-                      color: '#1890ff',
-                      fontSize: 14,
-                    }}
-                  >
-                    Market Stats
-                  </h3>
-                  <p className="stat">
-                    <span>Market Price</span>
-                    {price.toLocaleString('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 4,
-                    })}{' '}
-                    ({priceChange > 0 ? '+' : ''}
-                    {priceChange.toLocaleString()}%)
-                  </p>
-                  <p className="stat">
-                    <span>Volume (24hr):</span>
-                    {volume.toLocaleString('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 2,
-                    })}
-                  </p>
-                  <p className="stat">
-                    <span>Circulating Supply:</span>
-                    {circulatingSupply.toLocaleString('en-US', {
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 0,
-                    })}{' '}
-                    HNT
-                  </p>
-                  <p className="stat">
-                    <span>Maximum Supply:</span>
-                    223,000,000 HNT
-                  </p>
-                  <p className="stat">
-                    <span>Data Credits spent (30d):</span>
-                    {dataCredits.toLocaleString()} DC
-                  </p>
-                  <p className="stat">
-                    <span>Market Cap:</span>
-                    {marketCap.toLocaleString('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 2,
-                    })}
-                  </p>
-                </Col>
-              </Row>
-            </div>
-          </Fade>
+              <Col lg={12}>
+                <h3
+                  style={{
+                    marginBottom: 20,
+                    color: '#1890ff',
+                    fontSize: 14,
+                  }}
+                >
+                  Market Stats
+                </h3>
+                <p className="stat">
+                  <span>Market Price</span>
+                  {price.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 4,
+                  })}{' '}
+                  ({priceChange > 0 ? '+' : ''}
+                  {priceChange.toLocaleString()}%)
+                </p>
+                <p className="stat">
+                  <span>Volume (24hr):</span>
+                  {volume.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 2,
+                  })}
+                </p>
+                <p className="stat">
+                  <span>Circulating Supply:</span>
+                  {circulatingSupply.toLocaleString('en-US', {
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}{' '}
+                  HNT
+                </p>
+                <p className="stat">
+                  <span>Maximum Supply:</span>
+                  223,000,000 HNT
+                </p>
+                <p className="stat">
+                  <span>Data Credits spent (30d):</span>
+                  {dataCredits.toLocaleString()} DC
+                </p>
+                <p className="stat">
+                  <span>Market Cap:</span>
+                  {marketCap.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 2,
+                  })}
+                </p>
+              </Col>
+            </Row>
+            {/* <div className="flex-responsive"> */}
+            <Row>
+              <Col lg={12}>
+                <h3
+                  style={{
+                    marginBottom: 0,
+                    color: '#1890ff',
+                    fontSize: 14,
+                  }}
+                >
+                  Coverage Map
+                </h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <a href="/coverage">
+                  <MiniCoverageMap zoomLevel={0.65} />
+                </a>
+              </Col>
+            </Row>
+          </div>
 
           {/*<div style={{ position: 'relative', width: '100%' }}>
             <BarChart />

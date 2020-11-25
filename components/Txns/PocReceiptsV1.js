398aae87a7fcb863bc97c7999105a332825812be
Daniel James <danielcolinjames@gmail.com>
2020-11-25 12:21:26 -0800

    Make table columns equal widths

--- components/Txns/PocReceiptsV1.js
+++ components/Txns/PocReceiptsV1.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Descriptions, Collapse, Table, Tooltip } from 'antd'
 
-import { h3Distance, h3GetResolution } from 'h3-js'
+import { h3Distance } from 'h3-js'
 import Link from 'next/link'
 import animalHash from 'angry-purple-tiger'
 import PocPath from './PocPath'
@@ -138,8 +138,10 @@ const PocReceiptsV1 = ({ txn, h3exclusionCells, h3maxHopCells }) => (
                       </div>
                       {p.receipt && p.receipt.origin === 'radio' && (
                         <span className="poc-witness-receive-info" style={{}}>
-                          {`— received at RSSI ${p.receipt.signal}dBm, SNR ${
-                            p.receipt.snr ? `${p.receipt.snr.toFixed(2)}dB` : ''
+                          {`— received at RSSI ${p.receipt?.signal}dBm, SNR ${
+                            p.receipt.snr
+                              ? `${p.receipt?.snr.toFixed(2)}dB`
+                              : ''
                           }${
                             p.receipt !== null &&
                             p.receipt.datarate !== undefined
@@ -218,16 +220,6 @@ const PocReceiptsV1 = ({ txn, h3exclusionCells, h3maxHopCells }) => (
                                     w.location,
                                   )
 
-                                  // console.log(
-                                  //   `h3 res p: ${h3GetResolution(pDistance)}`,
-                                  // )
-                                  // console.log(
-                                  //   `h3 res w: ${h3GetResolution(w.location)}`,
-                                  // )
-                                  // console.log(
-                                  //   `Witness distance in h3 res 12 cells: ${witnessDistInH3Res12Cells}`,
-                                  // )
-
                                   // We can assume the diameter of 1 hexagon is roughly equal to its edge length * 2
                                   // The average edge length of a resolution-12 hexagon in h3 is given in km here: https://h3geo.org/docs/core-library/restable
                                   const avgRes12HexEdgeLengthInKm = 0.009415526
@@ -238,10 +230,6 @@ const PocReceiptsV1 = ({ txn, h3exclusionCells, h3maxHopCells }) => (
                                     avgRes12HexDiameterInKm *
                                     witnessDistInH3Res12Cells
 
-                                  // console.log(
-                                  //   `Witness distance in km: ~${witnessDistInKm}`,
-                                  // )
-
                                   const h3DistanceMinValid =
                                     h3exclusionCells <=
                                     witnessDistInH3Res12Cells
@@ -251,73 +239,84 @@ const PocReceiptsV1 = ({ txn, h3exclusionCells, h3maxHopCells }) => (
                                   const h3DistanceIsValid =
                                     h3DistanceMinValid && h3DistanceMaxValid
 
-                                  const columns =
+                                  const columns = []
+
+                                  let columnCount = 2 // because RSSI and distance should always be there
+                                  const snrIncluded = w.snr
+                                  const dataRateIncluded =
                                     w.datarate !== undefined &&
                                     ((Array.isArray(w.datarate) &&
                                       w.datarate.length > 0) ||
                                       (!Array.isArray(w.datarate) &&
                                         w.datarate !== null))
-                                      ? [
-                                          {
-                                            title: (
-                                              <PoCTableHeader
-                                                title="RSSI"
-                                                tooltipText={
-                                                  'RSSI stands for Received Signal Strength Indicator, and it represents the strength of the signal'
-                                                }
-                                              />
-                                            ),
-                                            dataIndex: 'rssi',
-                                          },
-                                          {
-                                            title: (
-                                              <PoCTableHeader
-                                                title="SNR"
-                                                tooltipText={
-                                                  'SNR stands for Signal-to-Noise Ratio, and it represents the quality of the signal'
-                                                }
-                                              />
-                                            ),
-                                            dataIndex: 'snr',
-                                          },
-                                          {
-                                            title: (
-                                              <PoCTableHeader
-                                                title="Distance"
-                                                tooltipText={`This value is an approximation of the distance between the hotspot that witnessed the challenge and the one that participated in it. Helium uses hexagons from the H3 library, so this distance is a rough approximation based on how many resolution 12 H3 cells the two hotspots are apart. E.g. if it says the distance is 0, it's because they are in the same cell.`}
-                                              />
-                                            ),
-                                            dataIndex: 'distance',
-                                          },
-                                          {
-                                            title: (
-                                              <PoCTableHeader
-                                                title="Data rate"
-                                                tooltipText={`The data rate at which the signal was received.`}
-                                              />
-                                            ),
-                                            dataIndex: 'datarate',
-                                          },
-                                        ]
-                                      : [
-                                          {
-                                            title: 'RSSI',
-                                            dataIndex: 'rssi',
-                                          },
-                                          {
-                                            title: 'SNR',
-                                            dataIndex: 'snr',
-                                          },
-                                          {
-                                            title: 'Distance',
-                                            dataIndex: 'distance',
-                                          },
-                                        ]
+
+                                  if (snrIncluded) columnCount++
+                                  if (dataRateIncluded) columnCount++
+
+                                  const columnWidth = `${
+                                    (1 / columnCount) * 100
+                                  }%`
+
+                                  const rssiColumn = {
+                                    title: (
+                                      <PoCTableHeader
+                                        title="RSSI"
+                                        tooltipText={
+                                          'RSSI stands for Received Signal Strength Indicator, and it represents the strength of the signal'
+                                        }
+                                      />
+                                    ),
+                                    dataIndex: 'rssi',
+                                    width: columnWidth,
+                                  }
+                                  const snrColumn = snrIncluded
+                                    ? {
+                                        title: (
+                                          <PoCTableHeader
+                                            title="SNR"
+                                            tooltipText={
+                                              'SNR stands for Signal-to-Noise Ratio, and it represents the quality of the signal'
+                                            }
+                                          />
+                                        ),
+                                        dataIndex: 'snr',
+                                        width: columnWidth,
+                                      }
+                                    : null
+                                  const distanceColumn = {
+                                    title: (
+                                      <PoCTableHeader
+                                        title="Distance"
+                                        tooltipText={`This value is an approximation of the distance between the hotspot that witnessed the challenge and the one that participated in it. Helium uses hexagons from the H3 library, so this distance is a rough approximation based on how many resolution 12 H3 cells the two hotspots are apart. E.g. if it says the distance is 0, it's because they are in the same cell.`}
+                                      />
+                                    ),
+                                    dataIndex: 'distance',
+                                    width: columnWidth,
+                                  }
+                                  const dataRateColumn = dataRateIncluded
+                                    ? {
+                                        title: (
+                                          <PoCTableHeader
+                                            title="Data rate"
+                                            tooltipText={`The data rate at which the signal was received.`}
+                                          />
+                                        ),
+                                        dataIndex: 'datarate',
+                                        width: columnWidth,
+                                      }
+                                    : null
+
+                                  columns.push(rssiColumn)
+                                  if (snrIncluded) columns.push(snrColumn)
+                                  columns.push(distanceColumn)
+                                  if (dataRateIncluded)
+                                    columns.push(dataRateColumn)
+
                                   const data = [
                                     {
                                       key: '1',
-                                      rssi: `${w.signal}dBm`,
-                                      snr: `${w.snr.toFixed(2)}dB`,
+                                      rssi: `${w?.signal}dBm`,
+                                      snr: `${w.snr?.toFixed(2)}dB`,
                                       distance: (
                                         <span
                                           style={

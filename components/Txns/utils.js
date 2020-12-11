efe55c2dca3a1ef9c3befe55961197d2d9dd02cf
Daniel James <danielcolinjames@gmail.com>
2020-12-11 12:53:42 -0800

    Use Mapbox fitBounds to zoom and pan to right spot

--- components/Txns/utils.js
+++ components/Txns/utils.js
@@ -1,32 +1,20 @@
-export const findMiddlePoint = (memberLocations) => {
-  let minLng = memberLocations[0].lng
-  let maxLng = memberLocations[0].lng
-  let minLat = memberLocations[0].lat
-  let maxLat = memberLocations[0].lat
+export const findBounds = (arrayOfLatsAndLons) => {
+  let minLon = arrayOfLatsAndLons[0].lng
+  let maxLon = arrayOfLatsAndLons[0].lng
+  let minLat = arrayOfLatsAndLons[0].lat
+  let maxLat = arrayOfLatsAndLons[0].lat
 
-  memberLocations.map((m) => {
-    if (m.lng < minLng) minLng = m.lng
-    if (m.lng > maxLng) maxLng = m.lng
+  arrayOfLatsAndLons.map((m) => {
+    if (m.lng < minLon) minLon = m.lng
+    if (m.lng > maxLon) maxLon = m.lng
     if (m.lat < minLat) minLat = m.lat
     if (m.lat > maxLat) maxLat = m.lat
   })
 
-  const distanceBetweenLons = maxLng - minLng
-  const distanceBetweenLats = maxLat - minLat
+  const mapBounds = [
+    [maxLon, maxLat],
+    [minLon, minLat],
+  ]
 
-  const midPointLon = maxLng - distanceBetweenLons / 2
-  const midPointLat = maxLat - distanceBetweenLats / 2
-
-  const zoomLevel =
-    distanceBetweenLons > 175
-      ? 0
-      : distanceBetweenLons > 100
-      ? 1.5
-      : distanceBetweenLons > 75
-      ? 2
-      : distanceBetweenLons > 60
-      ? 2.5
-      : 3
-
-  return [midPointLon, midPointLat, zoomLevel]
+  return mapBounds
 }

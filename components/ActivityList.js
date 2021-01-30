1af380a17526b0a433895f5319a63a8f88a7474f
Daniel James <danielcolinjames@gmail.com>
2021-01-29 19:08:40 -0800

    Show token burn amount in HNT

--- components/ActivityList.js
+++ components/ActivityList.js
@@ -227,8 +227,7 @@ const columns = (ownerAddress) => {
           </span>
         )
       case 'token_burn_v1':
-        const amountObject = new Balance(txn.amount, CurrencyType.networkToken)
-        return <span>{amountObject.toString(2)}</span>
+        return <span>{txn.amount.toString(2)}</span>
 
       case 'rewards_v1':
         return <span>{txn.totalAmount.toString(2)}</span>

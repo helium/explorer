508f1861006d123730fcf978fc3000349daeb77a
Philippe Schommers <philippe@schommers.be>
2021-02-08 20:25:16 +0100

    Replace React with Preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
+const withPreact = require('next-plugin-preact')
 
 // withCss required since react-mapbox-gl imports CSS directly within node_modules
-module.exports = withCSS(
-  withImages({
-    css: {
-      importAsGlobal: true,
-    },
-  }),
+module.exports = withPreact(
+  withCSS(
+    withImages({
+      css: {
+        importAsGlobal: true,
+      },
+    }),
+  ),
 )

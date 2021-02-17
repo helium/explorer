e8bdf2a96b083af68e95a2fa70392553316016b6
Daniel James <danielcolinjames@gmail.com>
2021-02-16 22:52:21 -0800

    Pull more recent consensus groups

--- data/consensus.js
+++ data/consensus.js
@@ -4,7 +4,7 @@ import Client from '@helium/http'
 export const fetchElections = async () => {
   const client = new Client()
   const list = await client.elections.list()
-  const elections = await list.take(10)
+  const elections = await list.take(20)
 
   // TODO: convert this to helium-js
   const res = await fetch(`https://api.helium.io/v1/hotspots/elected`)

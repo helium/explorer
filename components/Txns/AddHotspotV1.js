3547f770ccb0ae78ec529b1778490e8bd302380a
Daniel James <danielcolinjames@gmail.com>
2021-03-09 17:12:50 -0500

    Remove unnecessary fragment

--- components/Txns/AddHotspotV1.js
+++ components/Txns/AddHotspotV1.js
@@ -17,50 +17,48 @@ const AddHotspotV1 = ({ txn }) => {
   const feeObject = new Balance(txn.fee.integerBalance, CurrencyType.dataCredit)
 
   return (
-    <>
-      <Descriptions bordered>
-        <Descriptions.Item label="Hotspot" span={3}>
+    <Descriptions bordered>
+      <Descriptions.Item label="Hotspot" span={3}>
+        <div className="flex flex-row items-center justify-start">
+          <Link href={`/hotspots/${txn.gateway}`}>
+            <a>{animalHash(txn.gateway)}</a>
+          </Link>
+        </div>
+      </Descriptions.Item>
+      <Descriptions.Item label="Owner" span={3}>
+        <div className="flex flex-row items-center justify-start">
+          <AccountIcon address={txn.owner} className="mr-2" />
+          <Link href={`/accounts/${txn.owner}`}>
+            <a>{txn.owner}</a>
+          </Link>
+        </div>
+      </Descriptions.Item>
+      <Descriptions.Item label="Fee" span={3}>
+        <div className="flex flex-row items-center justify-start">
+          {feeObject.toString()}
+        </div>
+      </Descriptions.Item>
+      <Descriptions.Item label="Staking Fee" span={3}>
+        <div className="flex flex-row items-center justify-start">
+          {stakingFeeObject.toString()}
+        </div>
+      </Descriptions.Item>
+      <Descriptions.Item label="Staking Fee Payer" span={3}>
+        <div className="flex flex-col items-start justify-center">
           <div className="flex flex-row items-center justify-start">
-            <Link href={`/hotspots/${txn.gateway}`}>
-              <a>{animalHash(txn.gateway)}</a>
+            <AccountIcon address={stakingFeePayer} className="mr-2" />
+            <Link href={`/accounts/${stakingFeePayer}`}>
+              <a>{stakingFeePayer}</a>
             </Link>
           </div>
-        </Descriptions.Item>
-        <Descriptions.Item label="Owner" span={3}>
-          <div className="flex flex-row items-center justify-start">
-            <AccountIcon address={txn.owner} className="mr-2" />
-            <Link href={`/accounts/${txn.owner}`}>
-              <a>{txn.owner}</a>
-            </Link>
-          </div>
-        </Descriptions.Item>
-        <Descriptions.Item label="Fee" span={3}>
-          <div className="flex flex-row items-center justify-start">
-            {feeObject.toString()}
-          </div>
-        </Descriptions.Item>
-        <Descriptions.Item label="Staking Fee" span={3}>
-          <div className="flex flex-row items-center justify-start">
-            {stakingFeeObject.toString()}
-          </div>
-        </Descriptions.Item>
-        <Descriptions.Item label="Staking Fee Payer" span={3}>
-          <div className="flex flex-col items-start justify-center">
-            <div className="flex flex-row items-center justify-start">
-              <AccountIcon address={stakingFeePayer} className="mr-2" />
-              <Link href={`/accounts/${stakingFeePayer}`}>
-                <a>{stakingFeePayer}</a>
-              </Link>
-            </div>
-            <span className="pt-2">
-              {txn.payer === txn.owner || txn.payer === null
-                ? '(Hotspot owner)'
-                : '(Staking server)'}
-            </span>
-          </div>
-        </Descriptions.Item>
-      </Descriptions>
-    </>
+          <span className="pt-2">
+            {txn.payer === txn.owner || txn.payer === null
+              ? '(Hotspot owner)'
+              : '(Staking server)'}
+          </span>
+        </div>
+      </Descriptions.Item>
+    </Descriptions>
   )
 }
 

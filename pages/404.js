4e5b3a72b362d0c8d1bd22bb00a859e8369a40d4
Daniel James <danielcolinjames@gmail.com>
2021-03-12 00:07:45 -0500

    Update 404 page styles

--- pages/404.js
+++ pages/404.js
@@ -1,61 +1,100 @@
 import Link from 'next/link'
 import AppLayout from '../components/AppLayout'
+import ExternalLinkIcon from '../components/Icons/ExternalLink'
+import InternalLinkIcon from '../components/Icons/InternalLink'
+
+const HelpLinkList = ({ children }) => (
+  <div className="max-w-sm mx-auto">
+    <ul
+      role="list"
+      className="list-none m-0 p-0 flex flex-col items-center justify-center"
+    >
+      {children}
+    </ul>
+  </div>
+)
+const HelpLink = ({ children, href, external }) => {
+  if (external) {
+    return (
+      <a
+        target="_blank"
+        rel="noopener"
+        rel="noreferrer"
+        href={href}
+        className="w-full py-4 px-5  mb-4 bg-white shadow-md transition-all hover:shadow-xl rounded-md"
+      >
+        <li className="text-navy-600 font-semibold w-full m-0 p-0">
+          <div className="flex flex-row justify-between items-center">
+            {children}
+            <ExternalLinkIcon className="h-5 w-auto text-gray-300" />
+          </div>
+        </li>
+      </a>
+    )
+  } else {
+    return (
+      <Link href={href}>
+        <a className="w-full py-4 px-5  mb-4 bg-white shadow-md transition-all hover:shadow-xl rounded-md">
+          <li className="text-navy-600 font-semibold w-full m-0 p-0">
+            <div className="flex flex-row justify-between items-center">
+              {children}
+              <InternalLinkIcon className="h-5 w-auto text-gray-300" />
+            </div>
+          </li>
+        </a>
+      </Link>
+    )
+  }
+}
 
 const Custom404 = () => {
   return (
     <>
       <AppLayout>
-        <div className="h-full bg-bluegray-100 w-full">
-          <main className="pt-10 md:pt-20 w-full mx-auto max-w-3xl px-10 md:px-20">
+        <div className="bg-navy-500 w-full">
+          <div className="py-10 md:py-20 w-full mx-auto max-w-3xl px-10 md:px-20">
             <div className="flex flex-col items-center justify-start text-center">
-              <h1 className="font-sans font-semibold text-4xl md:text-6xl">
+              <h1 className="m-0 p-0 font-sans font-semibold text-white text-4xl md:text-6xl">
                 404
               </h1>
-              <h2 className="font-sans text-lg md:text-2xl">
-                Sorry, we can't find the page you're looking for.
+              <h2 className="m-0 p-0 pt-5 font-sans text-white text-lg md:text-2xl">
+                Sorry, that page doesn't seem to exist.
               </h2>
             </div>
-            <div className="">
-              <h3 className="text-gray-300">
-                Maybe one of these links will get you where you're going?
-              </h3>
-              <ul className="list-none">
-                <li>
-                  <Link href="https://www.helium.com/">
-                    <a>helium.com</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/coverage">
-                    <a>Coverage Map</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="https://www.helium.com/mine">
-                    <a>Mine</a>
-                  </Link>
-                </li>
-              </ul>
-              <h3 className="App-header-white-2">
-                Or if you think you shouldn't be seeing this error, please feel
-                free to:
-              </h3>
-              <ul>
-                <li>
-                  <Link href="https://github.com/helium/explorer/issues/new">
-                    <a>Create a GitHub issue</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="https://discord.com/invite/helium">
-                    <a>Report the issue in the #explorer channel on Discord</a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="errorFiller"></div>
-          </main>
+          </div>
         </div>
+        <div className="bg-bluegray-100 w-full">
+          <div className="py-10 md:pt-20 w-full mx-auto max-w-3xl px-10 md:px-20">
+            <h3 className="text-gray-300 font-sans font-normal text-lg text-center normal-case tracking-normal">
+              Let us know how you got here so we can fix it
+            </h3>
+            <HelpLinkList>
+              <HelpLink
+                href="https://github.com/helium/explorer/issues/new"
+                external
+              >
+                Create an issue on GitHub
+              </HelpLink>
+              <HelpLink href="https://discord.com/invite/helium" external>
+                Report the issue on Discord
+              </HelpLink>
+            </HelpLinkList>
+            <h3 className="text-gray-300 font-sans font-normal text-lg text-center normal-case tracking-normal pt-10">
+              Or try one of these links
+            </h3>
+            <HelpLinkList>
+              <HelpLink href="/hotspots">Hotspots</HelpLink>
+              <HelpLink href="/hotspots">Blocks</HelpLink>
+              <HelpLink href="/market">Market Data</HelpLink>
+              <HelpLink href="/consensus">Consensus</HelpLink>
+              <HelpLink href="/coverage">Coverage Map</HelpLink>
+              <HelpLink href="https://www.helium.com/" external>
+                helium.com
+              </HelpLink>
+            </HelpLinkList>
+          </div>
+        </div>
+        <div className="errorFiller"></div>
       </AppLayout>
     </>
   )

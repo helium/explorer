a6d69a3f25103820fddfd0cec4d7741deb3582c8
Andrew Allen <allenan@helium.com>
2021-04-06 13:11:59 -0700

    Fix/redis tls (#302)
    * use the redis tls env var
    * fix coverage map

--- pages/api/coverage.js
+++ pages/api/coverage.js
@@ -1,21 +1,5 @@
-import { getCoverage } from '../../commonjs/coverage'
-import Redis from 'ioredis'
-
-let redisClient
-if (process.env.REDIS_URL) {
-  redisClient = new Redis(process.env.REDIS_URL)
-}
-
-const getCache = async (key, fallback) => {
-  if (redisClient) {
-    const cachedValue = await redisClient.get(key)
-    if (cachedValue) {
-      return JSON.parse(cachedValue)
-    }
-  }
-
-  return fallback()
-}
+const { getCache } = require('../../commonjs/redis')
+const { getCoverage } = require('../../commonjs/coverage')
 
 export default async function handler(req, res) {
   const coverage = await getCache('coverage', getCoverage)

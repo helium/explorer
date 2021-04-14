25b3a90f310e5b096b5c9129165e5a5a731a8449
Andrew Allen <allenan@helium.com>
2021-04-14 12:17:42 -0700

    remove existing pages to lighten the build load

--- pages/index.js
+++ pages/index.js
@@ -1,244 +1,145 @@
-import React from 'react'
-import { Row, Col, Card, Button } from 'antd'
-import AppLayout, { Content } from '../components/AppLayout'
-import { fetchMarket, useMarket } from '../data/market'
-import { fetchStats, useStats } from '../data/stats'
-import { fetchOraclePrices, useOraclePrices } from '../data/oracles'
+import React, { useCallback, useState } from 'react'
+import Header from '../components/Beta/Header'
+import Page from '../components/CoverageMap/Page'
 import dynamic from 'next/dynamic'
-import OraclePriceChart from '../components/Oracles/OraclePriceChart'
-import OracleImg from '../public/images/oracle.svg'
-import Widget from '../components/Home/Widget'
-import round from 'lodash/round'
-import TopChart from '../components/AppLayout/TopChart'
-import TopBanner from '../components/AppLayout/TopBanner'
-import { getUnixTime, formatDistanceToNow } from 'date-fns'
-import HalvingCountdown from '../components/Home/HalvingCountdown'
-import BlocksList from '../components/BlocksList'
-import Link from 'next/link'
+import MetaTags from '../components/AppLayout/MetaTags'
+import InfoBox from '../components/Beta/InfoBox'
+import useToggle from '../utils/useToggle'
+import MapLayersBox from '../components/Beta/MapLayersBox'
+import MapControls from '../components/Beta/MapControls'
+import { haversineDistance } from '../utils/location'
+import { useHotspotsStats } from '../data/hotspots'
 
-const MiniCoverageMap = dynamic(
-  () => import('../components/CoverageMap/MiniCoverageMap'),
-  {
-    ssr: false,
-    loading: () => <div style={{ height: '500px' }} />,
-  },
-)
+const MAX_WITNESS_DISTANCE_THRESHOLD = 200
 
-const Index = ({
-  market: initialMarket,
-  stats: initialStats,
-  oraclePrices: initialOraclePrices,
-}) => {
-  const { market } = useMarket(initialMarket)
-  const { stats } = useStats(initialStats)
-  const { oraclePrices } = useOraclePrices(initialOraclePrices)
+const Map = dynamic(() => import('../components/Beta/CoverageMap'), {
+  ssr: false,
+  loading: () => <div />,
+})
 
-  const latestOraclePrice = (oraclePrices[0].price / 100000000).toLocaleString(
-    'en-US',
-    {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 4,
-    },
+async function getWitnesses(hotspotid) {
+  const witnesses = await fetch(
+    `https://api.helium.io/v1/hotspots/${hotspotid}/witnesses`,
   )
+    .then((res) => res.json())
+    .then((json) => json.data.filter((w) => !(w.address === hotspotid)))
+  return witnesses
+}
+
+const Coverage = () => {
+  const [showInfoBox, toggleShowInfoBox, setShowInfoBox] = useToggle(true)
+  const [showMapLayers, toggleShowMapLayers, setShowMapLayers] = useToggle(
+    false,
+  )
+  const [currentPosition, setCurrentPosition] = useState()
+  const [loadingCurrentPosition, setLoadingCurrentPosition] = useState(false)
+  const [selectedHotspot, setSelectedHotspot] = useState()
+  const [layer, setLayer] = useState(null)
+  const { hotspotsStats } = useHotspotsStats()
+
+  const requestCurrentPosition = useCallback(() => {
+    setLoadingCurrentPosition(true)
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentPosition(position)
+        setLoadingCurrentPosition(false)
+      },
+      (error) => {
+        console.error(error)
+        setLoadingCurrentPosition(false)
+      },
+    )
+  }, [])
+
+  const handleSelectHotspot = useCallback(async (hotspot) => {
+    const fetchedWitnesses = await getWitnesses(hotspot.address)
+    const filteredWitnesses = fetchedWitnesses.filter(
+      (w) =>
+        haversineDistance(hotspot?.lng, hotspot?.lat, w.lng, w.lat) <=
+        MAX_WITNESS_DISTANCE_THRESHOLD,
+    )
+    setSelectedHotspot({ ...hotspot, witnesses: filteredWitnesses })
+    setShowInfoBox(true)
+  }, [])
+
+  const handleSelectLayer = useCallback((layer) => {
+    setLayer(layer)
+    setShowMapLayers(false)
+  }, [])
 
   return (
-    <AppLayout>
-      <TopBanner
-        title={
-          <span>
-            Welcome to
-            <br />
-            <span style={{ fontWeight: 600, color: '#32C48D' }}>
-              Helium Explorer
-            </span>
-          </span>
+    <Page className="overflow-hidden">
+      <MetaTags
+        title={'Coverage Map'}
+        description={`View an interactive map of the Helium network and all the hotspots currently active around the world`}
+        openGraphImageAbsoluteUrl={
+          'https://explorer.helium.com/images/og/coverage.png'
         }
+        url={'https://explorer.helium.com/coverage'}
       />
-      <TopChart
-        title="Oracle Price (30d)"
-        subtitle={`${latestOraclePrice} (${formatDistanceToNow(
-          new Date(oraclePrices[0].timestamp),
-          {
-            addSuffix: true,
-          },
-        )})`}
-        icon={OracleImg}
-        tooltip="The Oracle Price is the price used for on-chain burn transactions. Derived from the market price, Oracles submit prices periodically to stabilize market fluctuations"
-        chart={
-          <OraclePriceChart
-            data={oraclePrices
-              .map(({ timestamp, price }) => ({
-                time: getUnixTime(new Date(timestamp)),
-                price: price / 100000000,
-              }))
-              .reverse()}
-          />
+      <title>Helium Network - Coverage</title>
+      <Header activeNav="coverage" />
+      <Map
+        currentPosition={currentPosition}
+        selectedHotspot={selectedHotspot}
+        selectHotspot={handleSelectHotspot}
+        showInfoBox={showInfoBox}
+        layer={layer}
+      />
+      <InfoBox
+        showInfoBox={showInfoBox}
+        toggleShowInfoBox={toggleShowInfoBox}
+        showMapLayers={showMapLayers}
+        toggleShowMapLayers={toggleShowMapLayers}
+        selectedHotspot={selectedHotspot}
+        hotspotsStats={hotspotsStats}
+      />
+      <MapLayersBox
+        showInfoBox={showInfoBox}
+        toggleShowInfoBox={toggleShowInfoBox}
+        showMapLayers={showMapLayers}
+        toggleShowMapLayers={toggleShowMapLayers}
+        layer={layer}
+        setLayer={handleSelectLayer}
+      />
+      <MapControls
+        showInfoBox={showInfoBox}
+        toggleShowInfoBox={toggleShowInfoBox}
+        showMapLayers={showMapLayers}
+        toggleShowMapLayers={toggleShowMapLayers}
+        requestCurrentPosition={requestCurrentPosition}
+        loadingCurrentPosition={loadingCurrentPosition}
+      />
+
+      <style jsx global>{`
+        #__next,
+        #app,
+        #app article {
+          height: 100%;
         }
-      />
-      <Content
-        style={{
-          marginTop: 0,
-          background: '#F4F5F7',
-        }}
-      >
-        <div
-          style={{ margin: '0 auto', maxWidth: 850 + 40 }}
-          className="content-container"
-        >
-          <Row gutter={[20, 20]}>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Hotspots"
-                value={stats.totalHotspots.toLocaleString()}
-                tooltip="The Helium network is made up of thousands of hotspots providing coverage around the globe"
-                footer="View Hotspots"
-                href="/hotspots"
-              />
-            </Col>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Blocks"
-                value={stats.totalBlocks.toLocaleString()}
-                footer="View Blocks"
-                href="/blocks"
-              />
-            </Col>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Consensus Groups"
-                value={stats.consensusGroups.toLocaleString()}
-                footer="View Consensus Groups"
-                href="/consensus"
-              />
-            </Col>
-          </Row>
-          <div
-            style={{
-              background: '#fff',
-              borderRadius: 10,
-              marginBottom: 20,
-              paddingBottom: 30,
-            }}
-            className="ant-card-head"
-          >
-            <Row>
-              <Col lg={12}>
-                <div class="ant-card-head-title">Hotspot Map</div>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
-                <a href="/coverage">
-                  <MiniCoverageMap zoomLevel={0.65} />
-                </a>
-              </Col>
-            </Row>
-          </div>
-          <Row gutter={[20, 20]}>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Election Times (24h)"
-                value={`${round(stats.electionTimes.lastDay.avg / 60, 1)} min`}
-                change={
-                  round(stats.electionTimes.lastHour.avg / 60) -
-                  round(stats.electionTimes.lastDay.avg / 60)
-                }
-                changeSuffix=" min"
-                changeUpIsBad
-                footer="View Consensus Groups"
-                href="/consensus"
-              />
-            </Col>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Block Time"
-                value={`${round(stats.blockTimes.lastHour.avg)} sec`}
-                change={
-                  round(stats.blockTimes.lastHour.avg) -
-                  round(stats.blockTimes.lastDay.avg)
-                }
-                changeSuffix=" sec"
-                changeUpIsBad
-                tooltip="The target block time is 60 sec. The network will adjust up or down to maintain this target."
-                footer="View Blocks"
-                href="/blocks"
-              />
-            </Col>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Current Price"
-                value={market.price.toLocaleString('en-US', {
-                  style: 'currency',
-                  currency: 'USD',
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 4,
-                })}
-                change={market.priceChange}
-                changeSuffix="%"
-                changePrecision={1}
-                tooltip="Based on data provided by CoinGecko"
-                footer="View Market Data"
-                href="/market"
-              />
-            </Col>
-            <Col xs={24} md={16}>
-              <HalvingCountdown />
-            </Col>
-            <Col xs={24} md={8}>
-              <Widget
-                title="Data Credits Spent (30d)"
-                value={stats.dataCredits.toLocaleString()}
-                subtitle={
-                  ((stats.dataCredits * 24) / 1073741824).toFixed(3) + ' GB'
-                }
-                tooltip="Data credits are spent to send and receive data over the Helium Network. HNT are burned to receive DC."
-                footer="View Market Data"
-                href="/market"
-              />
-            </Col>
-          </Row>
-          <Card title="Latest Blocks" style={{ marginBottom: 60 }}>
-            <BlocksList pageSize={10} showButton={false} />
-            <Row justify="center" style={{ padding: '20px 0' }}>
-              <Link href="/blocks">
-                <a>
-                  <Button
-                    size="large"
-                    style={{
-                      backgroundColor: '#5850EB',
-                      color: 'white',
-                      borderRadius: 6,
-                    }}
-                  >
-                    View All Blocks
-                  </Button>
-                </a>
-              </Link>
-            </Row>
-          </Card>
-        </div>
-      </Content>
-    </AppLayout>
+
+        html,
+        body {
+          overscroll-behavior: none;
+        }
+      `}</style>
+    </Page>
   )
 }
 
-export async function getStaticProps() {
-  const [market, stats, oraclePrices] = await Promise.all([
-    fetchMarket(),
-    fetchStats(),
-    fetchOraclePrices(),
-  ])
+export default Coverage
 
-  return {
-    props: {
-      market,
-      stats,
-      oraclePrices,
-    },
-    revalidate: 10,
-  }
-}
+// export async function getStaticProps() {
+//   const client = new Client()
+//   const stats = await client.stats.get()
+//   const count = stats.counts.hotspots
 
-export default Index
+//   let props = {
+//     count,
+//   }
+
+//   return {
+//     props,
+//     revalidate: 60,
+//   }
+// }

a7515998d2afd649abb3090547987de91d11eb65
Daniel James <danielcolinjames@gmail.com>
2021-04-19 15:02:57 -0700

    Let /beacons build (#330)

--- data/beacons.js
+++ data/beacons.js
@@ -1,8 +1,8 @@
 import useSWR from 'swr'
-import Client from '@helium/http'
+import Client, { Network } from '@helium/http'
 
 export const fetchLatestBeacons = (count = 100) => async () => {
-  const client = new Client()
+  const client = new Client(Network.staging)
   const beacons = await (await client.challenges.list()).take(count)
 
   return JSON.parse(JSON.stringify(beacons))

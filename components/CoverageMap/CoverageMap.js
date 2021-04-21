ef382625ccf113d5c19448200b4e4ef8d3719bc1
Andrew Allen <allenan@helium.com>
2021-04-21 14:46:15 -0700

    fix maptiler style url

--- components/CoverageMap/CoverageMap.js
+++ components/CoverageMap/CoverageMap.js
@@ -339,7 +339,7 @@ const CoverageMap = ({ selectedHotspots, selectHotspots, showOffline }) => {
           </span>
         </button>
         <Mapbox
-          style="mapbox://styles/petermain/cjyzlw0av4grj1ck97d8r0yrk"
+          style="https://api.maptiler.com/maps/2469a8ae-f7e5-4ed1-b856-cd312538e33b/style.json?key=kNomjOqCRi7kEjO4HbFF"
           className="mapbox-object"
           center={center}
           zoom={zoom}

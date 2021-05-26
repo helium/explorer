56e56c85b89288d833253706aa459737c2145de4
Daniel James <danielcolinjames@gmail.com>
2021-05-26 11:56:52 -0700

    Add token burn txn detail view

--- components/InfoBox/TxnDetails/TokenBurnV1.js
+++ components/InfoBox/TxnDetails/TokenBurnV1.js
@@ -1,85 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { Descriptions, Skeleton } from 'antd'
-import Link from 'next/link'
-import AccountIcon from '../../../components/AccountIcon'
-import { Balance, CurrencyType } from '@helium/currency'
-import Client from '@helium/http'
-
-const InlineSkeleton = () => (
-  <span className="inline-skeleton-override">
-    <Skeleton active paragraph={{ rows: 0 }} size="small" />
-  </span>
-)
+import { useState } from 'react'
+import { useAsync } from 'react-async-hook'
+import client from '../../../data/client'
+import InfoBoxPaneContainer from '../Common/InfoBoxPaneContainer'
+import AccountWidget from '../../Widgets/AccountWidget'
+import Widget from '../../Widgets/Widget'
 
 const TokenBurnV1 = ({ txn }) => {
-  const txnAmountObject = new Balance(
-    txn.amount.integerBalance,
-    CurrencyType.networkToken,
-  )
-  const txnFeeObject = new Balance(
-    txn.fee.integerBalance,
-    CurrencyType.dataCredit,
-  )
-
   const [oraclePrice, setOraclePrice] = useState()
-  useEffect(async () => {
-    const client = new Client()
+  useAsync(async () => {
     const { price } = await client.oracle.getPriceAtBlock(txn.height)
     setOraclePrice(price)
   }, [])
 
   return (
-    <Descriptions bordered>
-      <Descriptions.Item
-        label="Payer"
-        span={3}
-        style={{ overflow: 'ellipsis' }}
-      >
-        <div style={{ display: 'flex' }}>
-          <AccountIcon
-            address={txn.payer}
-            style={{ marginRight: 4, maxHeight: 24 }}
-          />
-          <Link href={`/accounts/${txn.payer}`}>
-            <a>{txn.payer}</a>
-          </Link>
-        </div>
-      </Descriptions.Item>
-      <Descriptions.Item label="Payee" span={3}>
-        <div style={{ display: 'flex' }}>
-          <AccountIcon
-            address={txn.payee}
-            style={{ marginRight: 4, maxHeight: 24 }}
-          />
-          <Link href={`/accounts/${txn.payee}`}>
-            <a>{txn.payee}</a>
-          </Link>
-        </div>
-      </Descriptions.Item>
-      <Descriptions.Item label="Amount" span={3}>
-        {txnAmountObject.toString(2)}
-      </Descriptions.Item>
-      <Descriptions.Item label="Oracle Price" span={3}>
-        ${oraclePrice ? oraclePrice.toString(2) : <InlineSkeleton />}
-      </Descriptions.Item>
-      <Descriptions.Item label="Value" span={3}>
-        $
-        {oraclePrice ? (
-          txnAmountObject.toUsd(oraclePrice).toString(2)
-        ) : (
-          <InlineSkeleton />
-        )}
-      </Descriptions.Item>
-      <Descriptions.Item label="Fee" span={3}>
-        {txnFeeObject.toString()}
-      </Descriptions.Item>
-      <Descriptions.Item label="Nonce" span={3}>
-        {txn.nonce}
-      </Descriptions.Item>
-      <Descriptions.Item label="Memo" span={3}>
-        {txn.memo}
-      </Descriptions.Item>
-    </Descriptions>
+    <InfoBoxPaneContainer>
+      <AccountWidget title="Payer" address={txn.payer} />
+      <AccountWidget title="Payee" address={txn.payee} />
+      <Widget title="Amount" span={2} value={txn.amount.toString(2)} />
+      <Widget
+        title="Oracle Price"
+        isLoading={!oraclePrice}
+        value={oraclePrice?.toString(2)}
+      />
+      <Widget
+        title="Value"
+        isLoading={!oraclePrice}
+        value={
+          oraclePrice ? txn.amount.toUsd(oraclePrice).toString(2) : 'Loading...'
+        }
+      />
+      <Widget title="Nonce" value={txn.nonce} />
+      <Widget title="Memo" value={txn.memo} />
+    </InfoBoxPaneContainer>
   )
 }
 

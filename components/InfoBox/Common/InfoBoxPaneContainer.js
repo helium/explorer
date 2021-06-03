2269760cc386c10cc248cf29cc07c376ef4e4b1d
Andrew Allen <allenan@helium.com>
2021-06-03 09:21:55 -0700

    fix validators list (#426)

--- components/InfoBox/Common/InfoBoxPaneContainer.js
+++ components/InfoBox/Common/InfoBoxPaneContainer.js
@@ -1,11 +1,21 @@
 import classNames from 'classnames'
 
-const InfoBoxPaneContainer = ({ children, classes }) => {
+const InfoBoxPaneContainer = ({
+  children,
+  className,
+  span = 2,
+  padding = true,
+}) => {
   return (
     <div
       className={classNames(
-        'grid grid-flow-row grid-cols-2 gap-3 md:gap-4 p-4 md:p-8 overflow-y-scroll no-scrollbar',
-        classes,
+        'grid grid-flow-row overflow-y-scroll no-scrollbar',
+        className,
+        {
+          'grid-cols-1': span === 1,
+          'grid-cols-2 gap-3 md:gap-4': span === 2,
+          'p-4 md:p-8': padding,
+        },
       )}
     >
       {children}

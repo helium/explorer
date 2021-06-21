666e7472c868c80fc8adecf2855e34e10778e4fd
Daniel James <danielcolinjames@gmail.com>
2021-06-21 10:18:58 -0700

    Add distance scale to map

---
+++ components/Map/ScaleLegend.js
@@ -0,0 +1,31 @@
+import { ScaleControl } from 'react-mapbox-gl'
+import classNames from 'classnames'
+import useMapLayer from '../../hooks/useMapLayer'
+
+const ScaleLegend = ({ className }) => {
+  const { showMapLayers, mapLayer } = useMapLayer()
+  return (
+    <div className="pointer-events-none w-full h-screen absolute right-0 bottom-0">
+      <div
+        className={classNames('w-full absolute right-0 bottom-0', className, {
+          'hidden pointer-events-none': showMapLayers,
+          'mb-20 mr-14 md:mb-16 md:mr-20': mapLayer === 'rewardScale',
+          'mb-8 mr-12 md:mb-5': mapLayer !== 'rewardScale',
+        })}
+      >
+        <ScaleControl
+          style={{
+            backgroundColor: 'transparent',
+            color: 'white',
+            fontFamily: 'Inter',
+            fontSize: 14,
+            boxShadow: 'none',
+            border: 'none',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ScaleLegend

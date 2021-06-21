f41830312c2f4707cbb4a56fb8c0c58d2588979e
Daniel James <danielcolinjames@gmail.com>
2021-06-21 16:11:43 -0700

    Fix memoization of expandable list item

--- components/Lists/ActivityList/ExpandableActivityListItem.js
+++ components/Lists/ActivityList/ExpandableActivityListItem.js
@@ -25,87 +25,72 @@ const ExpandableListItem = ({
   const { selectTxn, clearSelectedTxn } = useSelectedTxn()
   const { selectHotspot, clearSelectedHotspot } = useSelectedHotspot()
 
-  const renderExpandableItem = useCallback(
-    () => (
-      <div
-        className="w-full flex flex-col"
-        onClick={() => {
-          if (!expanded) {
-            selectTxn(txn.hash)
-          } else {
-            clearSelectedTxn()
-            if (context === 'hotspot') selectHotspot(address)
-          }
-          toggleExpanded()
-        }}
-      >
-        <div className="w-full flex justify-between">
-          <div className="w-full">
-            <div className="text-sm md:text-base font-medium text-darkgray-800 font-sans">
-              {title}
-            </div>
-            <div className="flex items-center space-x-2 md:space-x-4 h-6 text-gray-525 text-xs md:text-sm whitespace-nowrap">
-              {subtitle}
-            </div>
-          </div>
-          <ActivityPill
-            onClick={toggleExpanded}
-            expandable
-            expanded={expanded}
-            className={classNames(
-              'flex flex-row pl-2 pr-1 py-0.5 font-sans rounded-l-full',
-              pillClasses,
-            )}
-            pillColor={pillColor}
-            pillSymbolClasses={pillSymbolClasses}
-            details={details}
-          />
-        </div>
-        {expanded && (
-          <div className="w-full py-2 flex flex-col justify-between">
-            {expandedContent}
-            <Link
-              to={linkTo}
-              // clear selected hotspot when navigating to selected transaction, this was causing a Mapbox error on mobile
-              onClick={clearSelectedHotspot}
-              className={classNames(
-                'w-full bg-gray-300 hover:bg-gray-350 transition-all duration-200 cursor-pointer rounded-lg mt-2 flex items-center justify-center',
-              )}
-            >
-              <p className="text-gray-600 font-sans text-sm p-2 m-0">
-                View transaction details
-              </p>
-              <ChevronIcon
-                className={classNames(
-                  'h-auto  text-gray-600 transition-all duration-200 w-4 transform rotate-90',
-                )}
-              />
-            </Link>
-          </div>
-        )}
-      </div>
-    ),
-    [
-      address,
-      clearSelectedHotspot,
-      clearSelectedTxn,
-      context,
-      details,
-      expanded,
-      expandedContent,
-      linkTo,
-      pillClasses,
-      pillColor,
-      pillSymbolClasses,
-      selectHotspot,
-      selectTxn,
-      subtitle,
-      title,
-      toggleExpanded,
-      txn.hash,
-    ],
-  )
+  const handleItemClick = useCallback(() => {
+    if (!expanded) {
+      selectTxn(txn.hash)
+    } else {
+      clearSelectedTxn()
+      if (context === 'hotspot') selectHotspot(address)
+    }
+    toggleExpanded()
+  }, [
+    address,
+    clearSelectedTxn,
+    context,
+    expanded,
+    selectHotspot,
+    selectTxn,
+    toggleExpanded,
+    txn.hash,
+  ])
 
-  return renderExpandableItem()
+  return (
+    <div className="w-full flex flex-col" onClick={handleItemClick}>
+      <div className="w-full flex justify-between">
+        <div className="w-full">
+          <div className="text-sm md:text-base font-medium text-darkgray-800 font-sans">
+            {title}
+          </div>
+          <div className="flex items-center space-x-2 md:space-x-4 h-6 text-gray-525 text-xs md:text-sm whitespace-nowrap">
+            {subtitle}
+          </div>
+        </div>
+        <ActivityPill
+          onClick={toggleExpanded}
+          expandable
+          expanded={expanded}
+          className={classNames(
+            'flex flex-row pl-2 pr-1 py-0.5 font-sans rounded-l-full',
+            pillClasses,
+          )}
+          pillColor={pillColor}
+          pillSymbolClasses={pillSymbolClasses}
+          details={details}
+        />
+      </div>
+      {expanded && (
+        <div className="w-full py-2 flex flex-col justify-between">
+          {expandedContent}
+          <Link
+            to={linkTo}
+            // clear selected hotspot when navigating to selected transaction, this was causing a Mapbox error on mobile
+            onClick={clearSelectedHotspot}
+            className={classNames(
+              'w-full bg-gray-300 hover:bg-gray-350 transition-all duration-200 cursor-pointer rounded-lg mt-2 flex items-center justify-center',
+            )}
+          >
+            <p className="text-gray-600 font-sans text-sm p-2 m-0">
+              View transaction details
+            </p>
+            <ChevronIcon
+              className={classNames(
+                'h-auto  text-gray-600 transition-all duration-200 w-4 transform rotate-90',
+              )}
+            />
+          </Link>
+        </div>
+      )}
+    </div>
+  )
 }
 export default ExpandableListItem

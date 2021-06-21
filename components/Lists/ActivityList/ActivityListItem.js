74fd5276819a44bedea024c3b0ec635ae8744589
Daniel James <danielcolinjames@gmail.com>
2021-06-21 13:38:05 -0700

    Componentize activity pill

--- components/Lists/ActivityList/ActivityListItem.js
+++ components/Lists/ActivityList/ActivityListItem.js
@@ -1,52 +1,16 @@
 import classNames from 'classnames'
-import useToggle from '../../../utils/useToggle'
-import ChevronIcon from '../../Icons/Chevron'
-
-const ExpandButtonPill = ({
-  className,
-  details,
-  pillSymbolClasses,
-  pillColor,
-}) => {
-  return (
-    <div className="cursor-pointer flex items-center justify-end">
-      <div
-        className={classNames('whitespace-nowrap hidden md:flex', className)}
-        style={{ backgroundColor: pillColor }}
-      >
-        <span className="m-0">{details}</span>
-      </div>
-      <div
-        className={classNames(
-          'px-2 md:pl-1 md:pr-2 py-0.5 flex items-center justify-center rounded-full md:rounded-l-none md:rounded-r-full w-7 h-5 md:h-6',
-          pillSymbolClasses,
-        )}
-        style={{ backgroundColor: pillColor }}
-      >
-        <ChevronIcon
-          className={classNames(
-            'transition-all duration-200 w-4 transform rotate-90 h-3',
-          )}
-        />
-      </div>
-    </div>
-  )
-}
+import ActivityPill from './ActivityPill'
 
 const ActivityListItem = ({
   title,
   subtitle,
   details,
-  linkTo,
   pillClasses,
   pillSymbolClasses,
-  expandedContent,
   pillColor,
 }) => {
-  const [expanded, toggleExpanded] = useToggle()
-
   return (
-    <div className="w-full flex flex-col" onClick={toggleExpanded}>
+    <div className="w-full flex flex-col">
       <div className="w-full flex justify-between">
         <div className="w-full">
           <div className="text-sm md:text-base font-medium text-darkgray-800 font-sans">
@@ -56,9 +20,7 @@ const ActivityListItem = ({
             {subtitle}
           </div>
         </div>
-        <ExpandButtonPill
-          onClick={toggleExpanded}
-          expanded={expanded}
+        <ActivityPill
           className={classNames(
             'flex flex-row pl-2 pr-1 py-0.5 font-sans rounded-l-full',
             pillClasses,
--- components/Lists/ActivityList/ExpandableActivityListItem.js
+++ components/Lists/ActivityList/ExpandableActivityListItem.js
@@ -4,43 +4,7 @@ import ChevronIcon from '../../Icons/Chevron'
 import { Link } from 'react-router-i18n'
 import useSelectedTxn from '../../../hooks/useSelectedTxn'
 import useSelectedHotspot from '../../../hooks/useSelectedHotspot'
-
-const ExpandButtonPill = ({
-  className,
-  details,
-  expanded,
-  pillSymbolClasses,
-  pillColor,
-}) => {
-  return (
-    <div className="cursor-pointer flex items-center justify-end">
-      <div
-        className={classNames('whitespace-nowrap hidden md:flex', className)}
-        style={{ backgroundColor: pillColor }}
-      >
-        <span className="m-0 pr-2 pl-1">{details}</span>
-      </div>
-      <span
-        className={classNames(
-          'px-2 md:pl-1 md:pr-2 py-0.5 flex items-center justify-center rounded-full md:rounded-l-none md:rounded-r-full w-7 relative',
-          pillSymbolClasses,
-        )}
-        style={{ backgroundColor: pillColor }}
-      >
-        <span className="bg-black opacity-20 hidden md:block absolute left-0 top-0 rounded-full md:rounded-l-none md:rounded-r-full w-7 h-full" />
-        <span className="text-white z-10 hidden md:block">
-          {expanded ? '-' : '+'}
-        </span>
-        <ChevronIcon
-          className={classNames(
-            'block md:hidden transition-all duration-200 w-4 transform h-3 text-white',
-            { 'rotate-0': expanded, 'rotate-180': !expanded },
-          )}
-        />
-      </span>
-    </div>
-  )
-}
+import ActivityPill from './ActivityPill'
 
 const ExpandableListItem = ({
   address,
@@ -81,8 +45,9 @@ const ExpandableListItem = ({
             {subtitle}
           </div>
         </div>
-        <ExpandButtonPill
+        <ActivityPill
           onClick={toggleExpanded}
+          expandable
           expanded={expanded}
           className={classNames(
             'flex flex-row pl-2 pr-1 py-0.5 font-sans rounded-l-full',

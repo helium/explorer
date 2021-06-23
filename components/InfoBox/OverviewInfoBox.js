0b802f2fe7e7e920f8cf36a11e7854ae9221e4b0
Andrew Allen <allenan@helium.com>
2021-06-23 12:29:02 -0700

    increase max height of infobox

--- components/InfoBox/OverviewInfoBox.js
+++ components/InfoBox/OverviewInfoBox.js
@@ -28,66 +28,62 @@ const OverviewInfoBox = () => {
 
   return (
     <InfoBox title="Helium Explorer">
-      <TabNavbar>
-        <TabPane title="Overview" key="1">
-          <InfoBoxPaneContainer>
-            <TrendWidget
-              title="Hotspots"
-              series={hotspots?.count}
-              isLoading={!hotspots}
-              linkTo="/hotspots"
-            />
-            <StatWidget
-              title="Block Height"
-              series={blocks?.height}
-              isLoading={!blocks}
-              linkTo="/blocks"
-            />
-            <Widget
-              title="Market Price"
-              tooltip="Based on data provided by CoinGecko"
-              value={<Currency value={market?.price} />}
-              change={round(market?.priceChange, 2)}
-              changeSuffix="%"
-              isLoading={!market}
-              linkTo="/market"
-            />
-            <Widget
-              title="DC Spent (30d)"
-              tooltip="Data Credits are spent for transaction fees and to send data over the Helium Network. HNT are burned to create DC."
-              value={
-                (Math.abs(Number(dataCredits?.totalMonth)) / 1.0e9).toFixed(2) +
-                ' bn'
-              }
-              change={<Currency value={dataCredits?.totalMonth * 0.00001} />}
-              isLoading={!stats}
-              linkTo="/market"
-            />
-            <Widget
-              title="HNT Staked"
-              tooltip="The amount of HNT being staked by Validators"
-              value={formatLargeNumber(totalStaked)}
-              change={<Currency value={market?.price * totalStaked} />}
-              isLoading={!market}
-              linkTo="/validators"
-            />
-            <HalveningCountdownWidget />
+      <InfoBoxPaneContainer>
+        <TrendWidget
+          title="Hotspots"
+          series={hotspots?.count}
+          isLoading={!hotspots}
+          linkTo="/hotspots"
+        />
+        <StatWidget
+          title="Block Height"
+          series={blocks?.height}
+          isLoading={!blocks}
+          linkTo="/blocks"
+        />
+        <Widget
+          title="Market Price"
+          tooltip="Based on data provided by CoinGecko"
+          value={<Currency value={market?.price} />}
+          change={round(market?.priceChange, 2)}
+          changeSuffix="%"
+          isLoading={!market}
+          linkTo="/market"
+        />
+        <Widget
+          title="DC Spent (30d)"
+          tooltip="Data Credits are spent for transaction fees and to send data over the Helium Network. HNT are burned to create DC."
+          value={
+            (Math.abs(Number(dataCredits?.totalMonth)) / 1.0e9).toFixed(2) +
+            ' bn'
+          }
+          change={<Currency value={dataCredits?.totalMonth * 0.00001} />}
+          isLoading={!stats}
+          linkTo="/market"
+        />
+        <Widget
+          title="HNT Staked"
+          tooltip="The amount of HNT being staked by Validators"
+          value={formatLargeNumber(totalStaked)}
+          change={<Currency value={market?.price * totalStaked} />}
+          isLoading={!market}
+          linkTo="/validators"
+        />
+        <HalveningCountdownWidget />
 
-            <Widget
-              title="Total Beacons"
-              value={stats?.challenges?.toLocaleString()}
-              isLoading={!stats}
-              linkTo="/beacons"
-            />
-            <Widget
-              title="Total Validators"
-              value={validators.length.toLocaleString()}
-              isLoading={!validators}
-              linkTo="/validators"
-            />
-          </InfoBoxPaneContainer>
-        </TabPane>
-      </TabNavbar>
+        <Widget
+          title="Total Beacons"
+          value={stats?.challenges?.toLocaleString()}
+          isLoading={!stats}
+          linkTo="/beacons"
+        />
+        <Widget
+          title="Total Validators"
+          value={validators.length.toLocaleString()}
+          isLoading={!validators}
+          linkTo="/validators"
+        />
+      </InfoBoxPaneContainer>
     </InfoBox>
   )
 }

2794603a4983f1ceea74b962c37bfee645ec2357
Daniel James <danielcolinjames@gmail.com>
2021-06-24 08:39:08 -0700

    Hide zoom controls on mobile if info box is shown

--- components/Map/ZoomControls.js
+++ components/Map/ZoomControls.js
@@ -1,8 +1,17 @@
+import classNames from 'classnames'
 import { ZoomControl } from 'react-mapbox-gl'
+import useInfoBox from '../../hooks/useInfoBox'
 
 const ZoomControls = () => {
+  const { showInfoBox } = useInfoBox()
+
   return (
-    <div className="relative top-14 md:top-20 right-2 md:right-3 zoom-controls">
+    <div
+      className={classNames(
+        'md:flex relative top-14 md:top-20 right-2 md:right-3 zoom-controls transition-all md:pointer-events-auto md:opacity-100',
+        { 'opacity-0 pointer-events-none': showInfoBox },
+      )}
+    >
       <ZoomControl
         style={{
           boxShadow: 'none',

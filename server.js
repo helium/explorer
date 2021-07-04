36a97393524c8c8079c1b62df64263cf64b0708c
Andrew Allen <allenan@helium.com>
2021-07-04 10:12:24 -0700

    use custom server.js to try and fix crashing (#517)

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const next = require('next')
+const path = require('path')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+const compression = require('compression')
+const url = require('url')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+
+// Multi-process to utilize all CPU cores.
+if (!dev && cluster.isMaster) {
+  console.log(`Node cluster master ${process.pid} is running`)
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`,
+    )
+  })
+} else {
+  const app = next({ dir: '.', dev })
+  const nextHandler = app.getRequestHandler()
+
+  app.prepare().then(() => {
+    const server = express()
+
+    if (!dev) {
+      console.log('in production server config')
+      server.enable('trust proxy')
+      server.use(compression())
+
+      // Enforce SSL & HSTS in production
+      server.use((req, res, nextPlug) => {
+        const proto = req.headers['x-forwarded-proto']
+        if (proto === 'https') {
+          res.set({
+            'Strict-Transport-Security': 'max-age=31557600', // one-year
+          })
+          return nextPlug()
+        }
+        res.redirect(`https://${req.headers.host}${req.url}`)
+      })
+    }
+
+    // Static files
+    // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+    server.use(
+      '/static',
+      express.static(path.join(__dirname, 'static'), {
+        maxAge: dev ? '0' : '365d',
+      }),
+    )
+
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({ extended: true }))
+
+    // Default catch-all renders Next app
+    server.get('*', (req, res) => {
+      // res.set({
+      //   'Cache-Control': 'public, max-age=3600'
+      // });
+      const parsedUrl = url.parse(req.url, true)
+      nextHandler(req, res, parsedUrl)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`Listening on http://localhost:${port}`)
+    })
+  })
+}

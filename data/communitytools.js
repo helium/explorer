67900cfe348bf18e042f88f2dabfd1ccc6155a9a
Daniel James <danielcolinjames@gmail.com>
2021-07-13 16:27:18 -0700

    Add styling and tools

---
+++ data/communitytools.js
@@ -0,0 +1,29 @@
+export const communityToolsList = [
+  {
+    name: 'MinerTax',
+    url: 'https://www.minertax.com/home?symbol=HNT',
+    description: 'Export CSV for mining and staking rewards',
+    tags: ['Data Export'],
+  },
+
+  {
+    name: 'Hotspotty',
+    url: 'https://hotspotty.net',
+    description: 'Hotspot Monitoring and Dashboard solution',
+    tags: ['Monitoring', 'Data Export'],
+  },
+
+  {
+    name: 'Helisum',
+    url: 'https://helisum.com',
+    description: 'Hotspot Monitoring and Dashboard solution',
+    tags: ['Monitoring', 'Data Export'],
+  },
+
+  {
+    name: 'helium-tax',
+    url: 'https://davetapley.com/helium-tax/',
+    description: 'Export CSV for mining and staking rewards',
+    tags: ['Data Export'],
+  },
+]

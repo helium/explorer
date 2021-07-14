a6197700d27f3ed8f81bcd25dc449ba8ac49acac
Daniel James <danielcolinjames@gmail.com>
2021-07-13 17:10:40 -0700

    Add template to tools list

--- data/communitytools.js
+++ data/communitytools.js
@@ -1,3 +1,5 @@
+// Hi there! You're probably here to add a Helium community tool to the list below. At the bottom of the list is a template for how a new list item should be added.
+
 export const communityToolsList = [
   {
     name: 'MinerTax',
@@ -5,25 +7,29 @@ export const communityToolsList = [
     description: 'Export CSV for mining and staking rewards',
     tags: ['Data Export'],
   },
-
   {
     name: 'Hotspotty',
     url: 'https://hotspotty.net',
     description: 'Hotspot Monitoring and Dashboard solution',
     tags: ['Monitoring', 'Data Export'],
   },
-
   {
     name: 'Helisum',
     url: 'https://helisum.com',
     description: 'Hotspot Monitoring and Dashboard solution',
     tags: ['Monitoring', 'Data Export'],
   },
-
   {
     name: 'helium-tax',
     url: 'https://davetapley.com/helium-tax/',
     description: 'Export CSV for mining and staking rewards',
     tags: ['Data Export'],
   },
+  // The lines below are a template that you can copy and paste and then populate to add a new tool to the list. All the possible tags are included in the template, so just remove the ones that don't apply, and please don't make up new ones. Make sure to remove the "//" at the beginning of each line before you finish, and please leave the template here for the next person to use.
+  // {
+  //   name: 'Example Tool Name',
+  //   url: 'https://www.example.com',
+  //   description: 'Easily get the animal name of a given hotspot address',
+  //   tags: ['Data Export', 'Monitoring'],
+  // },
 ]

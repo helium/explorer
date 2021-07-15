233beac99f2d97453c0896234ae3d04467aa573c
Daniel James <danielcolinjames@gmail.com>
2021-07-14 18:24:13 -0700

    Add RSSI, SNR, and datarate to PoC receipt details (#551)
    * Add RSSI, SNR, and datarate to PoC receipt details
    
    * Add distance, RSSI, SNR to activity list item
    
    * Small design tweak: make tab nav shorter on mobile
    
    * Tidy up
    
    * Add tooltips to witness data on PoC receipts

--- components/InfoBox/TxnDetails/BeaconDetailsPane.js
+++ components/InfoBox/TxnDetails/BeaconDetailsPane.js
@@ -1,4 +1,3 @@
-import classNames from 'classnames'
 import { useState } from 'react'
 import { useAsync } from 'react-async-hook'
 import animalHash from 'angry-purple-tiger'
@@ -10,6 +9,7 @@ import { formatDistance, calculateDistance } from '../../../utils/distance'
 import { Link } from 'react-router-i18n'
 import InfoBoxPaneContainer from '../Common/InfoBoxPaneContainer'
 import useSelectedHotspot from '../../../hooks/useSelectedHotspot'
+import { Tooltip } from 'antd'
 
 const BeaconDetailsPane = ({ txn }) => {
   const [challenger, setChallenger] = useState()
@@ -46,9 +46,9 @@ const WitnessesWidget = ({
 }) => {
   const { selectHotspot } = useSelectedHotspot()
   return (
-    <div className={classNames(`bg-gray-200 p-3 rounded-lg col-span-2`)}>
+    <div className="bg-gray-200 p-3 rounded-lg col-span-2">
       <div className="text-gray-600 text-sm leading-loose">
-        <span className="flex items-center justify-start">
+        <span className="flex items-center justify-start mb-3">
           <img
             alt=""
             src="/images/witness-yellow-mini.svg"
@@ -61,23 +61,78 @@ const WitnessesWidget = ({
         {witnesses.map((w) => {
           const [witnessLat, witnessLng] = h3ToGeo(w.location)
           return (
-            <div key={w.gateway} className="flex justify-between items-center">
+            <div
+              key={w.gateway}
+              className="flex justify-between items-start pb-4"
+            >
               <div>
                 <Link
                   to={`/hotspots/${w.gateway}`}
                   onClick={() => selectHotspot(w.gateway)}
-                  className="text-base leading-tight tracking-tight text-navy-1000 hover:text-navy-400 transition-all duration-150"
+                  className="text-base leading-tight tracking-tight text-navy-1000 hover:text-navy-400 transition-all duration-150 flex items-center pb-2"
                 >
                   {animalHash(w.gateway)}
                 </Link>
-                <div className="text-sm leading-tight tracking-tighter text-gray-600">
-                  {challengeeLon &&
-                    formatDistance(
-                      calculateDistance(
-                        [challengeeLon, challengeeLat],
-                        [witnessLng, witnessLat],
-                      ),
-                    )}
+                <div className="flex items-center text-sm leading-tight tracking-tighter text-gray-600 space-x-1.5">
+                  <div className="grid grid-cols-2 gap-y-1 gap-x-2">
+                    <span>Distance</span>
+                    <Tooltip
+                      title={
+                        "This value is an approximation of the distance between the hotspot that witnessed the challenge and the one that participated in it. Helium uses hexagons from the H3 library, so this distance is a rough approximation based on how many resolution 12 H3 cells the two hotspots are apart. E.g. if it says the distance is 0, it's because they are in the same cell."
+                      }
+                      placement={'right'}
+                    >
+                      <span className="text-gray-800 font-medium">
+                        {challengeeLon &&
+                          formatDistance(
+                            calculateDistance(
+                              [challengeeLon, challengeeLat],
+                              [witnessLng, witnessLat],
+                            ),
+                          )}
+                      </span>
+                    </Tooltip>
+
+                    <span>Datarate</span>
+                    <Tooltip
+                      title={'The data rate at which the signal was received.'}
+                      placement={'right'}
+                    >
+                      <span className="text-gray-800 font-medium">
+                        {w.datarate}
+                      </span>
+                    </Tooltip>
+
+                    <span>RSSI</span>
+                    <Tooltip
+                      title={
+                        'RSSI stands for Received Signal Strength Indicator, and it represents the strength of the signal.'
+                      }
+                      placement={'right'}
+                    >
+                      <span className="text-gray-800 font-medium">
+                        {w.signal?.toLocaleString(undefined, {
+                          maximumFractionDigits: 2,
+                        })}
+                        dBm
+                      </span>
+                    </Tooltip>
+
+                    <span>SNR</span>
+                    <Tooltip
+                      title={
+                        'SNR stands for Signal-to-Noise Ratio, and it represents the quality of the signal.'
+                      }
+                      placement={'right'}
+                    >
+                      <span className="text-gray-800 font-medium">
+                        {w.snr?.toLocaleString(undefined, {
+                          maximumFractionDigits: 2,
+                        })}
+                        dB
+                      </span>
+                    </Tooltip>
+                  </div>
                 </div>
               </div>
               <Pill

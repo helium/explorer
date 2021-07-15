4eb9fa4adae2960a9ea2cb557428f88b6d605d97
Andrew Allen <allenan@helium.com>
2021-07-15 09:23:11 -0700

    migrate additional election stats (#552)
    * migrate additional election stats
    
    * cleanup
    
    * add penalty score to validator list

---
+++ components/Widgets/ElectionTimeWidget.js
@@ -0,0 +1,39 @@
+import TrendWidget from './TrendWidget'
+import { round } from 'lodash'
+import { useState, useEffect } from 'react'
+import useApi from '../../hooks/useApi'
+
+const ElectionTimeWidget = () => {
+  let { data: blocks } = useApi('/metrics/blocks')
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [electionTimes, setElectionTimes] = useState()
+
+  useEffect(() => {
+    if (!!blocks) {
+      setIsLoading(true)
+      const electionTimesArray = blocks?.electionTimeDay
+      setElectionTimes(
+        electionTimesArray.map((et) => ({
+          ...et,
+          value: round(et.value / 60, 1),
+        })),
+      )
+      setIsLoading(false)
+    }
+  }, [blocks])
+
+  return (
+    <TrendWidget
+      title="Election Time (24hr)"
+      series={electionTimes}
+      valueSuffix=" min"
+      changeInitial="second_last"
+      changeSuffix=" min"
+      isLoading={isLoading}
+      periodLabel={`${blocks?.electionTimeDay.length} days`}
+    />
+  )
+}
+
+export default ElectionTimeWidget

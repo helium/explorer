b24818bf0b022f82f25afe59dee0d8e6faf171de
Daniel James <danielcolinjames@gmail.com>
2021-07-14 17:43:17 -0700

    Add community tools (#545)
    * WIP: Start adding community tools
    
    * Add styling and tools
    
    * Add link to /tools from activity list
    
    * Add template to tools list
    
    * Add line breaks
    
    * Add instructions on submitting PR

---
+++ data/communitytools.js
@@ -0,0 +1,46 @@
+// Hi there! You're probably here to add a Helium community tool to the list below.
+// At the bottom of the list is a template for how a new list item should be added.
+
+// When you're ready to submit, if you're using the GitHub editing interface,
+// select the second option at the bottom ("Create a new branch for this commit and
+// start a pull request") and then click "Propose changes" and one of the core
+// contributors will review it as soon as possible.
+
+export const communityToolsList = [
+  {
+    name: 'MinerTax',
+    url: 'https://www.minertax.com/home?symbol=HNT',
+    description: 'Export CSV for mining and staking rewards',
+    tags: ['Data Export'],
+  },
+  {
+    name: 'Hotspotty',
+    url: 'https://hotspotty.net',
+    description: 'Hotspot Monitoring and Dashboard solution',
+    tags: ['Monitoring', 'Data Export'],
+  },
+  {
+    name: 'Helisum',
+    url: 'https://helisum.com',
+    description: 'Hotspot Monitoring and Dashboard solution',
+    tags: ['Monitoring', 'Data Export'],
+  },
+  {
+    name: 'helium-tax',
+    url: 'https://davetapley.com/helium-tax/',
+    description: 'Export CSV for mining and staking rewards',
+    tags: ['Data Export'],
+  },
+  // The lines below are a template that you can copy and paste and then populate
+  // to add a new tool to the list.All the possible tags are included in the
+  // template, so just remove the ones that don't apply, and please don't make up
+  // new ones. Make sure to remove the "//" at the beginning of each line before
+  // you finish, and please leave the template here for the next person to use.
+
+  // {
+  //   name: 'Example Tool Name',
+  //   url: 'https://www.example.com',
+  //   description: 'Easily get the animal name of a given hotspot address',
+  //   tags: ['Data Export', 'Monitoring'],
+  // },
+]

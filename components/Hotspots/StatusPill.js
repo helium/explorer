992a2a5f2b2679e8392e976f7845bc6ec18c95eb
Andrew Allen <allenan@helium.com>
2021-07-19 10:32:54 -0700

    backport additional sync status fixes

--- components/Hotspots/StatusPill.js
+++ components/Hotspots/StatusPill.js
@@ -1,5 +1,4 @@
-import { memo } from 'react'
-import { Tooltip } from 'antd'
+import { memo, useMemo } from 'react'
 import classNames from 'classnames'
 import { useAsync } from 'react-async-hook'
 import { fetchHeightByTimestamp } from '../../data/blocks'
@@ -22,46 +21,34 @@ const StatusPill = ({ hotspot }) => {
     return height
   }, [hotspot.status.timestamp])
 
+  const value = useMemo(() => {
+    if (status === 'offline') {
+      return 'Offline'
+    }
+
+    if (
+      !hotspot?.status?.height ||
+      !syncHeight ||
+      hotspot.status.height - syncHeight >= SYNC_BUFFER_BLOCKS
+    ) {
+      return 'Syncing'
+    }
+
+    return 'Synced'
+  }, [hotspot.status.height, status, syncHeight])
+
   if (syncHeightLoading) return null
 
   return (
-    <Tooltip
-      placement="top"
-      title={`Hotspot is ${status}. ${
-        status === 'online' &&
-        syncHeight !== null &&
-        hotspot.block - syncHeight <= SYNC_BUFFER_BLOCKS
-          ? 'Synced.'
-          : status === 'online' && syncHeight !== null
-          ? `Syncing block ${syncHeight.toLocaleString()}. `
-          : 'Hotspot is not syncing. '
-      }${
-        status === 'online' &&
-        syncHeight !== null &&
-        hotspot.block - syncHeight >= SYNC_BUFFER_BLOCKS
-          ? `Blocks remaining: ${(
-              hotspot.block - syncHeight
-            ).toLocaleString()}.`
-          : ``
-      }`}
-    >
-      <div className="flex flex-row items-center justify-center py-0.5 px-2.5 bg-navy-600 rounded-full">
-        <div
-          className={classNames('h-2.5', 'w-2.5', 'rounded-full', {
-            'bg-green-500': status === 'online',
-            'bg-red-400': status === 'offline',
-          })}
-        />
-        <p className="text-gray-600 ml-2 mb-0">
-          {status === 'offline'
-            ? `Offline`
-            : hotspot.block - syncHeight >= SYNC_BUFFER_BLOCKS ||
-              syncHeight === null
-            ? `Syncing`
-            : `Synced`}
-        </p>
-      </div>
-    </Tooltip>
+    <div className="flex flex-row items-center justify-center py-0.5 px-2.5 bg-navy-600 rounded-full">
+      <div
+        className={classNames('h-2.5', 'w-2.5', 'rounded-full', {
+          'bg-green-500': status === 'online',
+          'bg-red-400': status === 'offline',
+        })}
+      />
+      <p className="text-gray-600 ml-2 mb-0">{value}</p>
+    </div>
   )
 }
 

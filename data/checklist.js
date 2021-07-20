297ecfcb41a1c305d7fc2ec6309e437f42bafeb1
Coco Tang <coco@helium.com>
2021-07-20 12:09:57 -0700

    updates poc interval

--- data/checklist.js
+++ data/checklist.js
@@ -101,7 +101,7 @@ export const getChecklistItems = (
       sortOrder: 2,
       title: 'Create a Challenge',
       infoTooltipText:
-        'Hotspots that are synced and online create a challenge automatically, every 480 blocks (~8 hours).',
+        'Hotspots that are synced and online create a challenge automatically, every 300 blocks (~5 hours).',
       detailText:
         activity.challengerTxn !== null
           ? `Hotspot issued a challenge ${(
@@ -109,7 +109,7 @@ export const getChecklistItems = (
             ).toLocaleString()} block${
               height - activity.challengerTxn.height === 1 ? '' : 's'
             } ago.`
-          : `Hotspot hasn’t issued a challenge yet. Hotspots create challenges automatically every 480 blocks (~8 hours).`,
+          : `Hotspot hasn’t issued a challenge yet. Hotspots create challenges automatically every 300 blocks (~5 hours).`,
       completed: activity.challengerTxn !== null,
     },
     {
@@ -147,7 +147,7 @@ export const getChecklistItems = (
             ).toLocaleString()} block${
               height - activity.challengeeTxn.height === 1 ? '' : 's'
             } ago.`
-          : `Hotspot hasn’t participated in a challenge yet. Hotspots are challenged every 480 blocks.`,
+          : `Hotspot hasn’t participated in a challenge yet. Hotspots are challenged every 300 blocks.`,
       infoTooltipText:
         'Participation in a challenge depends on having witnesses. Use the checkbox to see Hotspots in your list. It can take a few days for challenges to include this Hotspot once a witness list is built.',
       completed: activity.challengeeTxn !== null,

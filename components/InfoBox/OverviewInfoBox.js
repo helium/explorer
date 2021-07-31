32accecf63be586eeb333a16f4d84191239adf4b
Daniel James <danielcolinjames@gmail.com>
2021-07-31 15:42:16 -0700

    Make halving widget delete itself when completed

--- components/InfoBox/OverviewInfoBox.js
+++ components/InfoBox/OverviewInfoBox.js
@@ -2,6 +2,7 @@ import { round } from 'lodash'
 import InfoBox from './InfoBox'
 import TrendWidget from '../Widgets/TrendWidget'
 import StatWidget from '../Widgets/StatWidget'
+import HalveningCountdownWidget from '../Widgets/HalvingCountdownWidget'
 import useApi from '../../hooks/useApi'
 import InfoBoxPaneContainer from './Common/InfoBoxPaneContainer'
 import { formatLargeNumber } from '../../utils/format'
@@ -65,6 +66,7 @@ const OverviewInfoBox = () => {
           isLoading={!market || !validatorStats}
           linkTo="/validators"
         />
+        <HalveningCountdownWidget />
         <Widget
           title="Total Beacons"
           value={stats?.challenges?.toLocaleString()}

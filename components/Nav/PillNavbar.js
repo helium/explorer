eace467f81377c3e95d1f5b73f76cd00feddfb0d
Daniel James <danielcolinjames@gmail.com>
2021-08-13 00:20:50 -0700

    Add scroll indicators to activity filter pill nav

--- components/Nav/PillNavbar.js
+++ components/Nav/PillNavbar.js
@@ -1,6 +1,9 @@
 import classNames from 'classnames'
 import { useCallback } from 'react'
 import { getTxnTypeColor } from '../../utils/txns'
+import { useScrollIndicators } from '../../hooks/useScrollIndicators'
+import ScrollIndicator from '../Nav/ScrollIndicator'
+import { useRef } from 'react'
 
 const NavItem = ({ title, active = false, onClick, type }) => {
   const handleClick = useCallback(() => {
@@ -25,19 +28,46 @@ const NavItem = ({ title, active = false, onClick, type }) => {
 }
 
 const PillNavbar = ({ navItems, activeItem, onClick }) => {
+  const scrollContainer = useRef(null)
+
+  const {
+    autoScroll,
+    isScrollable,
+    isScrolledToStart,
+    isScrolledToEnd,
+    updateScrollIndicators,
+  } = useScrollIndicators(scrollContainer)
   return (
-    <div className="flex px-2 md:px-4 py-2 md:py-3 bg-white overflow-x-scroll no-scrollbar border-b border-gray-400 border-solid">
-      {navItems.map((item) => (
-        <NavItem
-          key={item.key}
-          title={item.key}
-          type={item.value[0]}
-          active={item.key === activeItem}
-          onClick={onClick}
-        />
-      ))}
-      <span className="pr-4" />
-    </div>
+    <>
+      <div
+        className="flex px-2 md:px-4 py-2 md:py-3 bg-white overflow-x-scroll no-scrollbar border-b border-gray-400 border-solid"
+        ref={scrollContainer}
+        onScroll={updateScrollIndicators}
+      >
+        {navItems.map((item) => (
+          <NavItem
+            key={item.key}
+            title={item.key}
+            type={item.value[0]}
+            active={item.key === activeItem}
+            onClick={onClick}
+          />
+        ))}
+        <span className="pr-4" />
+      </div>
+      <ScrollIndicator
+        direction="right"
+        wrapperClasses="pb-1"
+        onClick={autoScroll}
+        shown={isScrollable && !isScrolledToEnd}
+      />
+      <ScrollIndicator
+        direction="left"
+        wrapperClasses="pb-1"
+        onClick={() => autoScroll({ direction: 'left' })}
+        shown={isScrollable && !isScrolledToStart}
+      />
+    </>
   )
 }
 

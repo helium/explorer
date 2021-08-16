fdb93ddc3a329c093ba8770587b36f043623a8ff
Coco Tang <cocotang@MacBook-Pro.attlocal.net>
2021-08-16 10:23:54 -0700

    closes #644

--- data/communitytools.js
+++ data/communitytools.js
@@ -17,6 +17,13 @@ export const communityToolsList = [
     tags: ['Monitoring'],
   },
   {
+    name: 'DeWi Rewards Report',
+    url: 'https://etl.dewi.org/dashboard/11-rewards-report-for-an-account',
+    description:
+      'A free rewards report for accounts to tabulate mining earnings. To login, username/pass=test@test.com/test123 ',
+    tags: ['Data Export'],
+  },
+  {
     name: 'Helistats',
     url: 'https://apps.apple.com/us/app/helistats/id1573119107',
     description:

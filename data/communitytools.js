435300aac87f5b341ad40ccb918c250c1190d4b3
Coco Tang <cocotang@MacBook-Pro.attlocal.net>
2021-08-17 13:03:43 -0700

    Add fairspot export to tools

--- data/communitytools.js
+++ data/communitytools.js
@@ -24,6 +24,12 @@ export const communityToolsList = [
     tags: ['Data Export'],
   },
   {
+    name: 'Fairspot CSV Export',
+    url: 'https://www.fairspot.host/hnt-export-mining-tax',
+    description: 'A tool to export transactions from your Helium wallet.',
+    tags: ['Data Export'],
+  },
+  {
     name: 'Helistats',
     url: 'https://apps.apple.com/us/app/helistats/id1573119107',
     description:

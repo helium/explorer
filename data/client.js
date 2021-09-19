805667f87aab3859640865e28c9aa556024ce5f3
Abhay Kumar <abhay@opensynapse.net>
2021-09-19 16:32:04 -0700

    wip: gordon

--- data/client.js
+++ data/client.js
@@ -2,6 +2,9 @@ import Client, { Network } from '@helium/http'
 
 export const TAKE_MAX = 100000
 
-const client = new Client(Network.production, { retry: 0 })
+const client = new Client(
+  new Network({ baseURL: 'http://gordon.stakejoy.com:8080', version: 1 }),
+  { retry: 0 },
+)
 
 export default client

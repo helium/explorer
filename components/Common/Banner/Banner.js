da9a963a632a62ee633a60b478a172cd241b51aa
Daniel James <danielcolinjames@gmail.com>
2021-09-24 10:29:46 -0700

    Make banner prettier

--- components/Common/Banner/Banner.js
+++ components/Common/Banner/Banner.js
@@ -7,17 +7,17 @@ const Banner = () => {
     <div className="fixed top-0 mx-auto w-full z-50">
       <div className="relative w-full">
         <a
-          className="bg-navy-400 w-full hover:shadow-lg hover:bg-navy-500 flex items-center pr-12 justify-center transition-all px-5 duration-250 text-white text-xs md:text-sm py-1 font-sans font-normal md:h-8 h-12 leading-tight"
+          className="bg-navy-400 w-full hover:shadow-lg hover:bg-navy-500 flex items-center pr-8 justify-center transition-all px-0.5 md:px-5 duration-250 text-white text-xs md:text-sm py-1 font-sans font-normal md:h-8 h-8 leading-tight"
           href="https://helium-explorer-pr-702.herokuapp.com/"
           rel="noopener noreferrer"
           target="_blank"
         >
           <p className="m-0">
-          Help stress test the new Explorer with the faster API! {'->'}
+            Help stress test the new Explorer with the faster API! {'->'}
           </p>
         </a>
         <button
-          className="border border-solid absolute right-0 top-0 border-transparent hover:bg-navy-600 w-10 h-full z-40 cursor-pointer bg-navy-500 focus:outline-none focus:border-navy-400 flex items-center justify-center"
+          className="border border-solid absolute right-0 top-0 border-transparent hover:bg-navy-600 w-8 md:w-10 h-full z-40 cursor-pointer bg-navy-500 focus:outline-none focus:border-navy-400 flex items-center justify-center"
           onClick={hideBanner}
         >
           <svg

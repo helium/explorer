f6b0689a36600a959c3791e8429e7592947e51cd
Andrew Allen <allenan@helium.com>
2021-10-01 12:13:07 -0400

    re-enable helium-js retry

--- data/client.js
+++ data/client.js
@@ -4,7 +4,7 @@ export const TAKE_MAX = 100000
 
 const client = new Client(
   new Network({ baseURL: 'https://helium-api.stakejoy.com', version: 1 }),
-  { retry: 0 },
+  { retry: 3 },
 )
 
 export default client

cd003209134dafb8d4ff507184513d5e2b4120fa
Daniel James <danielcolinjames@gmail.com>
2021-12-01 14:36:16 -0800

    Use client env var

--- data/client.js
+++ data/client.js
@@ -2,9 +2,10 @@ import Client, { Network } from '@helium/http'
 
 export const TAKE_MAX = 100000
 
-const client = new Client(
-  new Network({ baseURL: 'https://helium-api.stakejoy.com', version: 1 }),
-  { retry: 3 },
-)
+const baseURL = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL
+  : 'https://helium-api.stakejoy.com'
+
+const client = new Client(new Network({ baseURL, version: 1 }), { retry: 3 })
 
 export default client

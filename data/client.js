5dd547c171df052e63634e8d1d6a9679596ece4e
Daniel James <danielcolinjames@gmail.com>
2021-12-01 15:44:40 -0800

    Fix env var

--- data/client.js
+++ data/client.js
@@ -2,8 +2,8 @@ import Client, { Network } from '@helium/http'
 
 export const TAKE_MAX = 100000
 
-const baseURL = process.env.CLIENT_URL
-  ? process.env.CLIENT_URL
+const baseURL = process.env.NEXT_PUBLIC_CLIENT_URL
+  ? process.env.NEXT_PUBLIC_CLIENT_URL
   : 'https://helium-api.stakejoy.com'
 
 const client = new Client(new Network({ baseURL, version: 1 }), { retry: 3 })

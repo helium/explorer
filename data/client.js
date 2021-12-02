9acc75eab5fa34085ca5560b968d3bde65979c53
Daniel James <danielcolinjames@gmail.com>
2021-12-01 16:29:27 -0800

    yolo

--- data/client.js
+++ data/client.js
@@ -2,9 +2,7 @@ import Client, { Network } from '@helium/http'
 
 export const TAKE_MAX = 100000
 
-const baseURL = process.env.NEXT_PUBLIC_CLIENT_URL
-  ? process.env.NEXT_PUBLIC_CLIENT_URL
-  : 'https://helium-api.stakejoy.com'
+const baseURL = 'https://yolo.skittles.stakejoy.com'
 
 const client = new Client(new Network({ baseURL, version: 1 }), { retry: 3 })
 

a9fdadf98148da1ce3d9d9f3fc0bfdd07d3cc29d
Matt Reetz <matthewcarlreetz@gmail.com>
2022-01-03 13:20:20 -0600

    Make banner update hex layer to earnings.

--- components/Common/Banner/Banner.js
+++ components/Common/Banner/Banner.js
@@ -1,19 +1,31 @@
+import { useCallback } from 'react'
 import { useContext } from 'react'
+import useMapLayer from '../../../hooks/useMapLayer'
 import BannerContext from './BannerContext'
 
 const Banner = () => {
   const { hideBanner } = useContext(BannerContext)
+  const { setMapLayer } = useMapLayer()
+
+  const updateMapLayer = useCallback(
+    (layer) => () => {
+      setMapLayer(layer)
+      hideBanner()
+    },
+    [hideBanner, setMapLayer],
+  )
+
   return (
     <div className="fixed top-0 mx-auto w-full z-50">
       <div className="relative w-full">
         <a
           className="bg-navy-400 w-full hover:shadow-lg hover:bg-navy-500 flex items-center pr-8 justify-center transition-all px-0.5 md:px-5 duration-250 text-white text-xs md:text-sm py-1 font-sans font-normal md:h-8 h-8 leading-tight"
-          href="https://docs.helium.com/blockchain/mining#how-do-hotspots-earn-helium-tokens"
+          onClick={updateMapLayer('earnings')}
           rel="noopener noreferrer"
-          target="_blank"
         >
-          <p className="m-0 mr-2">
-            Check out the new Hotspot Earnings Map Layer
+          <p className="m-0 mr-1">
+            Check out the new Earnings Map layer. Click the icon in the bottom
+            right corner
           </p>
 
           <svg
@@ -35,6 +47,8 @@ const Banner = () => {
               fill="white"
             />
           </svg>
+
+          <p className="m-0 mr-2 ml-1">and select Earnings.</p>
         </a>
         <button
           className="border border-solid absolute right-0 top-0 border-transparent hover:bg-navy-600 w-8 md:w-10 h-full z-40 cursor-pointer bg-navy-500 focus:outline-none focus:border-navy-400 flex items-center justify-center"

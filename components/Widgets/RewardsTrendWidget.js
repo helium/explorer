98643b9f16ed4b4712f0ce2c7dd86f99a3d1313b
Daniel James <danielcolinjames@gmail.com>
2022-01-05 13:29:23 -0500

    Add AM/PM to 24H earnings tooltip timestamp

--- components/Widgets/RewardsTrendWidget.js
+++ components/Widgets/RewardsTrendWidget.js
@@ -234,7 +234,7 @@ const RewardsTrendWidget = ({
       if (dataPointTimePeriod === 'day') {
         return format(offsetDate, 'MMM d')
       }
-      return `${format(offsetDate, 'MMM d hh:mm')} UTC`
+      return `${format(offsetDate, 'MMM d hh:mma')} UTC`
     })
 
     return {

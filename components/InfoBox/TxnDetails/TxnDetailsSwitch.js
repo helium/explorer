9f91657cab68f0eb9ddebaa5abf28f228de70cf4
Daniel James <danielcolinjames@gmail.com>
2022-01-21 10:55:45 -0800

    Clean up transaction details switch

--- components/InfoBox/TxnDetails/TxnDetailsSwitch.js
+++ components/InfoBox/TxnDetails/TxnDetailsSwitch.js
@@ -18,46 +18,52 @@ import {
   ValidatorHeartbeatV1,
 } from './index'
 
-const TxnDetailsSwitch = ({ txn, isLoading, inline = false }) => {
-  if (isLoading) return null
-
+const getTxnComponent = (txn) => {
   switch (txn.type) {
     case 'poc_receipts_v1':
-      return <BeaconDetailsPane txn={txn} />
+      return BeaconDetailsPane
     case 'payment_v1':
-      return <PaymentV1 txn={txn} inline={inline} />
+      return PaymentV1
     case 'payment_v2':
-      return <PaymentV2 txn={txn} inline={inline} />
+      return PaymentV2
     case 'poc_request_v1':
-      return <PocRequestV1 txn={txn} inline={inline} />
+      return PocRequestV1
     case 'rewards_v1':
     case 'rewards_v2':
     case 'rewards_v3':
-      return <Rewards txn={txn} />
+      return Rewards
     case 'consensus_group_v1':
-      return <ConsensusGroupV1 txn={txn} inline={inline} />
+      return ConsensusGroupV1
     case 'state_channel_close_v1':
-      return <StateChannelCloseV1 txn={txn} />
+      return StateChannelCloseV1
     case 'state_channel_open_v1':
-      return <StateChannelOpenV1 txn={txn} inline={inline} />
+      return StateChannelOpenV1
     case 'transfer_hotspot_v1':
-      return <TransferHotspotV1 txn={txn} inline={inline} />
+      return TransferHotspotV1
     case 'add_gateway_v1':
-      return <AddGatewayV1 txn={txn} inline={inline} />
+      return AddGatewayV1
     case 'assert_location_v1':
-      return <AssertLocationV1 txn={txn} inline={inline} />
+      return AssertLocationV1
     case 'assert_location_v2':
-      return <AssertLocationV2 txn={txn} inline={inline} />
+      return AssertLocationV2
     case 'token_burn_v1':
-      return <TokenBurnV1 txn={txn} inline={inline} />
+      return TokenBurnV1
     case 'stake_validator_v1':
-      return <StakeValidatorV1 txn={txn} inline={inline} />
+      return StakeValidatorV1
     case 'transfer_validator_stake_v1':
-      return <TransferValidatorStakeV1 txn={txn} inline={inline} />
+      return TransferValidatorStakeV1
     case 'validator_heartbeat_v1':
-      return <ValidatorHeartbeatV1 txn={txn} inline={inline} />
+      return ValidatorHeartbeatV1
     default:
-      return <Fallback txn={txn} inline={inline} />
+      return Fallback
   }
+}
+
+const TxnDetailsSwitch = ({ txn, isLoading, inline = false }) => {
+  if (isLoading) return null
+
+  const TxnComponent = getTxnComponent(txn)
+
+  return <TxnComponent txn={txn} inline={inline} />
 }
 export default TxnDetailsSwitch

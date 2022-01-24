beb5674a32c9d1ac11743f10c16b7434ba114042
Daniel James <danielcolinjames@gmail.com>
2022-01-24 12:12:16 -0800

    Componentize notification icon

--- components/Common/Changelog/ChangelogButton.js
+++ components/Common/Changelog/ChangelogButton.js
@@ -51,32 +51,7 @@ const ChangelogButton = ({ className }) => {
       )}
       onClick={showChangelog}
     >
-      {newChangelogItems?.length > 0 ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
-        </svg>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-          />
-        </svg>
-      )}
-      {newChangelogItems?.length > 0 && (
+      <NotificationIcon hasNewNotifications={newChangelogItems} />
         <div
           className={'absolute z-50 cursor-pointer -top-1 -right-1'}
           onClick={() => {

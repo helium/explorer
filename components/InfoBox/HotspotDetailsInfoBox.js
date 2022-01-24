dd9a38e0b155061d7034d0cac668321c4712b070
Daniel James <danielcolinjames@gmail.com>
2022-01-24 13:21:15 -0800

    Remove activity changelog indicator

--- components/InfoBox/HotspotDetailsInfoBox.js
+++ components/InfoBox/HotspotDetailsInfoBox.js
@@ -198,18 +198,7 @@ const HotspotDetailsInfoBox = ({ address, isLoading, hotspot }) => {
             <StatisticsPane hotspot={hotspot} isDataOnly={IS_DATA_ONLY} />
           )}
         </TabPane>
-        <TabPane
-          title="Activity"
-          path="activity"
-          key="activity"
-          changelogIndicator={
-            <ChangelogIndicator
-              changelogItemKey="activity-v2"
-              positionClasses="top-[290px] md:top-[250px] left-[140px] md:left-[160px]"
-              sizeClasses="w-4 h-4 md:w-4 md:h-4"
-            />
-          }
-        >
+        <TabPane title="Activity" path="activity" key="activity">
           {isLoading ? (
             <SkeletonActivityList />
           ) : (

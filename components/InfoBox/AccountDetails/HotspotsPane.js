3986e4983bb14d5495f802d9dfc22c1a0fdb83a3
Andrew Allen <allenan@helium.com>
2022-01-27 14:53:59 -0500

    add correct counts to hotpsot and validator lists

--- components/InfoBox/AccountDetails/HotspotsPane.js
+++ components/InfoBox/AccountDetails/HotspotsPane.js
@@ -1,8 +1,10 @@
 import classNames from 'classnames'
+import { useAccount } from '../../../data/accounts'
 import { useHotspots } from '../../../data/hotspots'
 import HotspotsList from '../../Lists/HotspotsList'
 
 const HotspotsPane = ({ address }) => {
+  const { account } = useAccount(address)
   const { hotspots, fetchMore, isLoadingInitial, isLoadingMore, hasMore } =
     useHotspots('account', address)
 
@@ -19,6 +21,7 @@ const HotspotsPane = ({ address }) => {
         isLoadingMore={isLoadingMore}
         fetchMore={fetchMore}
         hasMore={hasMore}
+        count={account?.hotspotCount}
         showCount
       />
     </div>

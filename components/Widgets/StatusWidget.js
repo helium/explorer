afecb778ba9fe413dd9ca2557b6ab4e3fc0cd5e0
Daniel James <danielcolinjames@gmail.com>
2022-02-23 15:51:54 -0800

    Update "Synced" to "Connected"

--- components/Widgets/StatusWidget.js
+++ components/Widgets/StatusWidget.js
@@ -2,40 +2,17 @@ import { memo, useMemo } from 'react'
 import classNames from 'classnames'
 import TimeAgo from 'react-time-ago'
 import Widget from './Widget'
-import { useAsync } from 'react-async-hook'
-import { fetchHeightByTimestamp } from '../../data/blocks'
-import { SYNC_BUFFER_BLOCKS } from '../Hotspots/utils'
 
 const StatusWidget = ({ hotspot }) => {
   const status = hotspot?.status?.online
-
-  const { result: syncHeight, loading: syncHeightLoading } =
-    useAsync(async () => {
-      const timestamp = hotspot?.status?.timestamp
-
-      if (!timestamp) {
-        return 1
-      }
-
-      const height = await fetchHeightByTimestamp(timestamp)
-      return height
-    }, [hotspot.status.timestamp])
 
   const value = useMemo(() => {
     if (status === 'offline') {
       return 'Offline'
     }
 
-    if (
-      !hotspot?.status?.height ||
-      !syncHeight ||
-      syncHeight - hotspot.status.height >= SYNC_BUFFER_BLOCKS
-    ) {
-      return 'Syncing'
-    }
-
-    return 'Synced'
-  }, [hotspot.status.height, status, syncHeight])
+    return 'Connected'
+  }, [status])
 
   return (
     <Widget
@@ -49,7 +26,6 @@ const StatusWidget = ({ hotspot }) => {
         )
       }
       tooltip="Hotspots gossip their sync status over the p2p network. Pair with a hotspot over Bluetooth to get the most up-to-date sync status."
-      isLoading={syncHeightLoading}
       icon={
         <div
           className={classNames('rounded-full w-5 h-5', {

4a19d9a7ad8011d229084432e3d2a3654f22933a
Daniel James <danielcolinjames@gmail.com>
2022-02-23 17:07:59 -0800

    Add average transmit scale to Witnessed section

--- components/InfoBox/HotspotDetails/WitnessedPane.js
+++ components/InfoBox/HotspotDetails/WitnessedPane.js
@@ -3,11 +3,27 @@ import { useAsync } from 'react-async-hook'
 import { fetchWitnessed } from '../../../data/hotspots'
 import WitnessedList from '../../Lists/WitnessedList'
 import animalHash from 'angry-purple-tiger'
+import Widget from '../../Widgets/Widget'
+import { useMemo } from 'react'
+import { generateRewardScaleColor } from '../../Hotspots/utils'
 
 const WitnessedPane = ({ hotspot }) => {
   const { result: witnessed, loading } = useAsync(fetchWitnessed, [
     hotspot.address,
   ])
+
+  const avgTransmitScale = useMemo(() => {
+    if (!witnessed) {
+      return 0
+    }
+
+    const totalTransmit = witnessed.reduce(
+      (acc, { rewardScale }) => acc + rewardScale,
+      0,
+    )
+
+    return totalTransmit / witnessed.length
+  }, [witnessed])
 
   return (
     <div
@@ -23,11 +39,27 @@ const WitnessedPane = ({ hotspot }) => {
         title="Witnessed"
         description={
           <>
-            <div>
-              Hotspots on the Helium network whose beacons{' '}
-              {animalHash(hotspot.address)} has successfully witnessed. There
-              are many reasons a Hotspot may not be a valid witness.
-            </div>
+            {!loading && (
+              <div>
+                Over the last 5 days, {animalHash(hotspot.address)} has
+                successfully witnessed beacons from{' '}
+                <span className="font-bold text-navy-400">
+                  {witnessed?.length} Hotspot
+                  {witnessed?.length === 1 ? '' : 's'}
+                </span>
+                {', '}
+                with an average transmit scale of{' '}
+                <span
+                  className="font-bold"
+                  style={{
+                    color: generateRewardScaleColor(avgTransmitScale),
+                  }}
+                >
+                  {avgTransmitScale.toFixed(2)}
+                </span>
+                .
+              </div>
+            )}
             <div className="pt-1.5">
               Learn more{' '}
               <a

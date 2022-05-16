bebed66a3bcbb88a1aad344b5fd3c682a597068a
Tyler Whitman <twhit093@gmail.com>
2022-05-16 15:05:40 -0700

    change hotspot status updated time to use last activity time

--- components/Widgets/StatusWidget.js
+++ components/Widgets/StatusWidget.js
@@ -1,4 +1,4 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 import classNames from 'classnames'
 import TimeAgo from 'react-time-ago'
 import Widget from './Widget'
@@ -6,11 +6,22 @@ import { useAsync } from 'react-async-hook'
 import { fetchHeightByTimestamp } from '../../data/blocks'
 import { SYNC_BUFFER_BLOCKS } from '../Hotspots/utils'
 import useChallengeIssuer from '../../hooks/useChallengeIssuer'
+import client from '../../data/client'
 
 const StatusWidget = ({ hotspot }) => {
   const status = hotspot?.status?.online
-
+  const [lastActivityTimestamp, setLastActivityTimestamp] = useState()
+  const [loadingActivityTimestamp, setLoadingActivityTimestamp] = useState(true)
   const { challengeIssuer, challengeIssuerLoading } = useChallengeIssuer()
+
+  useAsync(async () => {
+    setLoadingActivityTimestamp(true)
+    const transactionList = await (await client.hotspot(hotspot.address).roles.list()).take(1)
+    if (transactionList && transactionList[0] && transactionList[0].time) {
+      setLastActivityTimestamp(transactionList[0].time * 1000)
+    }
+    setLoadingActivityTimestamp(false)
+  }, [])
 
   const liteHotspotsActive = useMemo(() => {
     return challengeIssuer === 'validator'
@@ -53,13 +64,13 @@ const StatusWidget = ({ hotspot }) => {
       title="Status"
       value={value}
       subtitle={
-        hotspot?.status?.timestamp && (
+        lastActivityTimestamp && (
           <span className="text-gray-550 text-sm font-sans">
-            Last Updated <TimeAgo date={hotspot?.status?.timestamp} />
+            Last Updated <TimeAgo date={lastActivityTimestamp} />
           </span>
         )
       }
-      isLoading={syncHeightLoading || challengeIssuerLoading}
+      isLoading={syncHeightLoading || challengeIssuerLoading || loadingActivityTimestamp}
       tooltip="A Hotspot is online and synced if it has any blockchain activity in the last 36 hours (including Proof-of-Coverage, transferring packets, or receiving mining rewards)."
       icon={
         <div

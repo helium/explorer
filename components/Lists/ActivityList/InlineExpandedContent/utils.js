69a33a0da21be755a30acadc1d638720b9f08b4e
Matt Reetz <matthewcarlreetz@gmail.com>
2022-05-19 12:58:08 -0500

    Fix crash when clicking on hotspot activity item. (#955)

--- components/Lists/ActivityList/InlineExpandedContent/utils.js
+++ components/Lists/ActivityList/InlineExpandedContent/utils.js
@@ -2,7 +2,7 @@ export const isDefaultExpandedStyle = (txn) => {
   return !(
     // any transaction type where we need to use custom padding /
     // spacing / backgrounds to make a nice looking expanded view
-    txn?.type.startsWith('poc_receipts')
+    txn?.type?.startsWith('poc_receipts')
     // || txn?.type.startsWith('other_type')
   )
 }

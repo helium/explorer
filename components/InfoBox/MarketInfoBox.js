2e8e4a6fecfe93481e412194ac452a5dd79204c7
ervinchai <ervin.chai1108@gmail.com>
2022-08-04 14:21:53 +0800

    CoinGecko: Add backlinks to CoinGecko website

--- components/InfoBox/MarketInfoBox.js
+++ components/InfoBox/MarketInfoBox.js
@@ -46,7 +46,7 @@ const MarketInfoBox = () => {
             />
             <Widget
               title="Market Price"
-              tooltip="Based on data provided by CoinGecko"
+              tooltip={<span>Based on data provided by <a target="_blank" rel="noreferrer" href="https://www.coingecko.com/en/coins/helium">CoinGecko</a></span>}
               value={<Currency value={market?.price} />}
               change={round(market?.priceChange, 2)}
               changeSuffix="%"
@@ -54,7 +54,7 @@ const MarketInfoBox = () => {
             />
             <Widget
               title="Market Cap"
-              tooltip="Based on data provided by CoinGecko"
+              tooltip={<span>Based on data provided by <a target="_blank" rel="noreferrer" href="https://www.coingecko.com/en/coins/helium">CoinGecko</a></span>}
               value={
                 <Currency
                   value={market?.price * stats?.circulatingSupply}

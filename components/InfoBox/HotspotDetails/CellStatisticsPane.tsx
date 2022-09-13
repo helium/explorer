fad92389293cc1f51cb78c72025e279bc35d1f1e
Andrew Allen <allenan@helium.com>
2022-09-13 15:02:26 -0400

    add cell speedtests
    change

--- components/InfoBox/HotspotDetails/CellStatisticsPane.tsx
+++ components/InfoBox/HotspotDetails/CellStatisticsPane.tsx
@@ -4,6 +4,7 @@ import useApi from '../../../hooks/useApi'
 import CellStatusWidget from '../../Widgets/CellStatusWidget'
 import { Hotspot } from '@helium/http'
 import { SWRResponse } from 'swr'
+import CellSpeedtestWidget from '../../Widgets/CellSpeedtestWidget'
 
 export type CellHotspot = {
   blockTimestamp: string
@@ -17,21 +18,36 @@ export type CellHotspot = {
   txnHash: string
 }
 
+export type CellSpeedtest = {
+  downloadSpeed: number
+  hotspotAddress: string
+  latency: number
+  timestamp: number
+  uploadSpeed: number
+}
+
 type Props = {
   hotspot: Hotspot
 }
 
 const CellStatisticsPane = ({ hotspot }: Props) => {
-  const { data: cellHotspot }: SWRResponse<CellHotspot> =
-    useApi(`/cell/hotspots/${hotspot.address}`)
+  const { data: cellHotspot }: SWRResponse<CellHotspot> = useApi(
+    `/cell/hotspots/${hotspot.address}`,
+  )
+
+  const { data: cellSpeedtest }: SWRResponse<CellSpeedtest> = useApi(
+    `/cell/hotspots/${hotspot.address}/latest-speedtest`,
+  )
 
   return (
     <InfoBoxPaneContainer>
       <CellStatusWidget cellHotspot={cellHotspot} />
+      <CellSpeedtestWidget cellSpeedtest={cellSpeedtest} />
       <a
         href="https://docs.helium.com/5g-on-helium/cbrs-radios"
         target="_blank"
-        className="col-span-2" rel="noreferrer"
+        className="col-span-2"
+        rel="noreferrer"
       >
         For more information on Small Cell Radios and status, visit the docs.
       </a>
